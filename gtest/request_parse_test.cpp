5feca63653f35c96530e40ccb6f54ff7b0cbec06
yaito6502 <schwarzekatze8010110@gmail.com>
2022-08-18 22:18:34 +0900

    feat: 🎸 request-target部分のデータ構造を決定、実装

--- gtest/request_parse_test.cpp
+++ gtest/request_parse_test.cpp
@@ -2,32 +2,54 @@
 #include <algorithm>
 #include <cctype>
 
+enum RequestForm {
+	ORIGIN,
+	ABSOLUTE,
+	AUTHORITY,
+	ASTERISK,
+};
+
+class URI
+{
+  public:
+	std::string scheme_;
+	std::string userinfo_;
+	std::string host_;
+	unsigned int port_;
+	std::string path_;
+	std::string query_;
+	std::string fragment_;
+};
+
+class RequestTarget
+{
+  public:
+	RequestForm form_type_;
+	URI request_target_;
+};
+
 enum Methods {
 	GET,
 	POST,
 	DELETE,
 };
 
-// class RequestLine
-// {
-//   public:
-	// Methods method_;
-	// std::string request_target_;
-	// int http_version_;
-//
-	// RequestLine(Methods method, , int http_version) : method_(method), , http_version_(http_version) {
-//
-	// }
-// };
+class RequestLine
+{
+  public:
+	Methods method_;
+	RequestTarget request_target_;
+	int http_version_;
+};
 
-//class StatusLine
-//{
-//};
+// class StatusLine
+// {
+// };
 
 class StartLine
 {
   public:
-	//RequestLine request_line_;
+	RequestLine request_line_;
 	//StatusLine status_line_;
 };
 
@@ -37,8 +59,9 @@ class FieldLines
 	std::map<const std::string, std::string> field_lines_;
 
   public:
-	FieldLines(): field_lines_() {
-
+	FieldLines()
+		: field_lines_()
+	{
 	}
 
 	std::string &operator[](std::string field_name)
@@ -49,20 +72,23 @@ class FieldLines
 		return field_lines_[field_name];
 	}
 
-	bool operator==(const FieldLines& rhs) const {
+	bool operator==(const FieldLines &rhs) const
+	{
 		return field_lines_ == rhs.field_lines_;
 	}
 
-	bool operator!=(const FieldLines& rhs) const {
+	bool operator!=(const FieldLines &rhs) const
+	{
 		return !(*this == rhs);
 	}
 };
 
-TEST(request_parse, field_line) {
+TEST(request_parse, field_line)
+{
 	FieldLines field;
 	field["Content-Length"] = "11";
 	EXPECT_EQ(field["content-length"], "11");
-	field["Content-Length"] =  "12";
+	field["Content-Length"] = "12";
 	EXPECT_EQ(field["content-length"], "12");
 }
 
@@ -103,56 +129,56 @@ class HTTPMessage
 // std::string input = "GET / HTTP/1.1\r\n\r\n";
 //
 // ASSERT_EQ(
-//actualy
-	// HTTPMessage(input),
-	//expect
-	// HTTPMessage(
-	// "GET / HTTP/1.1",
-	// std::map<std::string, std::string>(),
-	// ""));
-	// }
+// actualy
+// HTTPMessage(input),
+// expect
+// HTTPMessage(
+// "GET / HTTP/1.1",
+// std::map<std::string, std::string>(),
+// ""));
+// }
 
-	// TEST(request_parse, post_with_no_field_line) {
-	// 	std::string input = "POST / HTTP/1.1\r\n\r\nHelloWorld!";
+// TEST(request_parse, post_with_no_field_line) {
+// 	std::string input = "POST / HTTP/1.1\r\n\r\nHelloWorld!";
 
-	// 	ASSERT_EQ(
-	// 		// actualy
-	// 		HTTPMessage(input),
-	// 		// expect
-	// 		HTTPMessage(
-	// 			"POST / HTTP/1.1",
-	// 			std::vector<std::string>(),
-	// 			"HelloWorld!")
-	// 	);
-	// }
+// 	ASSERT_EQ(
+// 		// actualy
+// 		HTTPMessage(input),
+// 		// expect
+// 		HTTPMessage(
+// 			"POST / HTTP/1.1",
+// 			std::vector<std::string>(),
+// 			"HelloWorld!")
+// 	);
+// }
 
-	// TEST(request_parse, post_with_single_field_line) {
-	// 	std::string input = "POST / HTTP/1.1\r\nContent-Length: 11\r\nHelloWorld!";
+// TEST(request_parse, post_with_single_field_line) {
+// 	std::string input = "POST / HTTP/1.1\r\nContent-Length: 11\r\nHelloWorld!";
 
-	// 	ASSERT_EQ(
-	// 		// actualy
-	// 		HTTPMessage(input),
-	// 		// expect
-	// 		HTTPMessage(
-	// 			"POST / HTTP/1.1",
-	// 			std::vector<std::string>{"Content-Length: 11"},
-	// 			"HelloWorld!")
-	// 	);
-	// }
+// 	ASSERT_EQ(
+// 		// actualy
+// 		HTTPMessage(input),
+// 		// expect
+// 		HTTPMessage(
+// 			"POST / HTTP/1.1",
+// 			std::vector<std::string>{"Content-Length: 11"},
+// 			"HelloWorld!")
+// 	);
+// }
 
-	// TEST(request_parse, post_with_multi_field_lines) {
-	// 	std::string input = "POST / HTTP/1.1\r\nConnection: keep-alive\r\nContent-Length: 11\r\nHelloWorld!";
+// TEST(request_parse, post_with_multi_field_lines) {
+// 	std::string input = "POST / HTTP/1.1\r\nConnection: keep-alive\r\nContent-Length: 11\r\nHelloWorld!";
 
-	// 	ASSERT_EQ(
-	// 		// actualy
-	// 		HTTPMessage(input),
-	// 		// expect
-	// 		HTTPMessage(
-	// 			"POST / HTTP/1.1",
-	// 			std::vector<std::string>{
-	// 				"Connection: keep-alive",
-	// 				"Content-Length: 11"
-	// 			},
-	// 			"HelloWorld!")
-	// 	);
-	// }
+// 	ASSERT_EQ(
+// 		// actualy
+// 		HTTPMessage(input),
+// 		// expect
+// 		HTTPMessage(
+// 			"POST / HTTP/1.1",
+// 			std::vector<std::string>{
+// 				"Connection: keep-alive",
+// 				"Content-Length: 11"
+// 			},
+// 			"HelloWorld!")
+// 	);
+// }
