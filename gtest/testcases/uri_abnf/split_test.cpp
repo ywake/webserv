5b2295e5b652113b691d10c8136e132401dacff9
nori <nori623p@gmail.com>
2022-08-23 10:42:45 +0200

    test: 💍 splitのテストケースを修正
    空文字列に分割されるようにexpectを変更

--- gtest/testcases/uri_abnf/split_test.cpp
+++ gtest/testcases/uri_abnf/split_test.cpp
@@ -12,23 +12,23 @@ TEST(uri_abnf, split_basic)
 TEST(uri_abnf, split_serial_delims)
 {
 	ABNF::StringAry act = ABNF::Split("Hello  World .", " ");
-	ABNF::StringAry exp = ABNF::StringAry({"Hello", "World", "."});
+	ABNF::StringAry exp = ABNF::StringAry({"Hello", "", "World", "."});
 
 	EXPECT_EQ(act, exp);
 }
 
 TEST(uri_abnf, split_head)
 {
-	ABNF::StringAry act = ABNF::Split(" Hello  World .", " ");
-	ABNF::StringAry exp = ABNF::StringAry({"Hello", "World", "."});
+	ABNF::StringAry act = ABNF::Split(" Hello World .", " ");
+	ABNF::StringAry exp = ABNF::StringAry({"", "Hello", "World", "."});
 
 	EXPECT_EQ(act, exp);
 }
 
 TEST(uri_abnf, split_tail)
 {
-	ABNF::StringAry act = ABNF::Split("Hello  World . ", " ");
-	ABNF::StringAry exp = ABNF::StringAry({"Hello", "World", "."});
+	ABNF::StringAry act = ABNF::Split("Hello World . ", " ");
+	ABNF::StringAry exp = ABNF::StringAry({"Hello", "World", ".", ""});
 
 	EXPECT_EQ(act, exp);
 }
@@ -44,7 +44,7 @@ TEST(uri_abnf, split_2_len_delims)
 TEST(uri_abnf, split_len_delims)
 {
 	ABNF::StringAry act = ABNF::Split("Hello World.", "Hello");
-	ABNF::StringAry exp = ABNF::StringAry({" World."});
+	ABNF::StringAry exp = ABNF::StringAry({"", " World."});
 
 	EXPECT_EQ(act, exp);
 }
@@ -68,7 +68,7 @@ TEST(uri_abnf, split_empty_delims)
 TEST(uri_abnf, split_all_of_string_is_delim)
 {
 	ABNF::StringAry act = ABNF::Split("          ", " ");
-	ABNF::StringAry exp = ABNF::StringAry({""});
+	ABNF::StringAry exp = ABNF::StringAry({"", "", "", "", "", "", "", "", "", "", ""});
 
 	EXPECT_EQ(act, exp);
 }
