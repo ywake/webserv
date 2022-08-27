e7200302bd870f2e0cf3ee19530743e585b8d0e3
ywake <ywake@student.42tokyo.jp>
2022-08-27 22:32:32 +0900

    test: 💍 IsHeirPartのテストのにFalseテストを追加

--- gtest/testcases/uri_abnf/is_heir_part_test.cpp
+++ gtest/testcases/uri_abnf/is_heir_part_test.cpp
@@ -12,14 +12,12 @@ TEST(uri_abnf, is_heir_part_authority_and_path_abempty)
 
 	EXPECT_TRUE(ABNF::IsHeirPart("//example.com:80//index.html"));
 
-	EXPECT_TRUE(ABNF::IsHeirPart("//ywake@example.com//index.html"));
-	EXPECT_TRUE(ABNF::IsHeirPart("//ywake:@example.com//index.html"));
-	EXPECT_TRUE(ABNF::IsHeirPart("//ywake:pass@example.com//index.html"));
-	EXPECT_TRUE(ABNF::IsHeirPart("//ywake:pass:@example.com//index.html"));
-	EXPECT_TRUE(ABNF::IsHeirPart("//ywake:pass:xxxx@example.com//index.html"));
-	EXPECT_TRUE(ABNF::IsHeirPart("//ywake:pass:xxxx@example.com:8080//index.html"));
-
-	EXPECT_FALSE(ABNF::IsHeirPart("///"));
+	EXPECT_TRUE(ABNF::IsHeirPart("//name@example.com//index.html"));
+	EXPECT_TRUE(ABNF::IsHeirPart("//name:@example.com//index.html"));
+	EXPECT_TRUE(ABNF::IsHeirPart("//name:pass@example.com//index.html"));
+	EXPECT_TRUE(ABNF::IsHeirPart("//name:pass:@example.com//index.html"));
+	EXPECT_TRUE(ABNF::IsHeirPart("//name:pass:xxxx@example.com//index.html"));
+	EXPECT_TRUE(ABNF::IsHeirPart("//name:pass:xxxx@example.com:8080//index.html"));
 }
 
 // path-absolute = "/" [segment-nz *("/" segment )]
@@ -28,7 +26,7 @@ TEST(uri_abnf, is_heir_part_path_absolute)
 	EXPECT_TRUE(ABNF::IsHeirPart("/example.com"));
 	EXPECT_TRUE(ABNF::IsHeirPart("/example.com/"));
 	EXPECT_TRUE(ABNF::IsHeirPart("/example.com/index.html"));
-	EXPECT_TRUE(ABNF::IsHeirPart("//example.com//index.html"));
+	EXPECT_TRUE(ABNF::IsHeirPart("/example.com//index.html"));
 }
 
 // path-rootless = segment-nz *("/" segment )
@@ -44,6 +42,10 @@ TEST(uri_abnf, is_heir_part_path_rootless)
 TEST(uri_abnf, is_heir_part_path_empty)
 {
 	EXPECT_TRUE(ABNF::IsHeirPart(""));
+}
 
+TEST(uri_abnf, is_heir_part_false_case)
+{
+	EXPECT_FALSE(ABNF::IsHeirPart("///"));
 	EXPECT_FALSE(ABNF::IsHeirPart("a"));
 }
