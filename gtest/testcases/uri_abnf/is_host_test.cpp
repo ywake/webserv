6bb10cc32d175e8daa3688851779cb0c68afef57
nori <nori623p@gmail.com>
2022-08-29 06:58:09 +0200

    test: 💍 ishostのテストケース追加

--- gtest/testcases/uri_abnf/is_host_test.cpp
+++ gtest/testcases/uri_abnf/is_host_test.cpp
@@ -3,8 +3,18 @@
 
 #include "uri_abnf.hpp"
 
-	TEST(uri_abnf, is_host_ipv6)
+TEST(uri_abnf, is_host_ipv6)
 {
 	EXPECT_TRUE(ABNF::IsHost("[::1]"));
+}
+
+TEST(uri_abnf, is_host_regname_true)
+{
+	EXPECT_TRUE(ABNF::IsHost("localhost"));
+}
+
+TEST(uri_abnf, is_host_regname_false)
+{
+	EXPECT_FALSE(ABNF::IsHost(""));
 }
 */
