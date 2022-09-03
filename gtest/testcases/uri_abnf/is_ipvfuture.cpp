67704e9213eb924ebfaa22c33fab0917b008d391
nori <nori623p@gmail.com>
2022-09-03 13:15:44 +0200

    test: 💍 ipvfutureのテストケースを追加

---
+++ gtest/testcases/uri_abnf/is_ipvfuture.cpp
@@ -0,0 +1,31 @@
+#include "gtest.h"
+
+#include "parse_ip.hpp"
+
+TEST(uri_abnf, is_ipvfuture_true)
+{
+	EXPECT_TRUE(ABNF::IsIPvFuture("vv.v"));
+	EXPECT_TRUE(ABNF::IsIPvFuture("vvvvvvvvvvv.v"));
+	EXPECT_TRUE(ABNF::IsIPvFuture("v1.1"));
+	EXPECT_TRUE(ABNF::IsIPvFuture("va.a"));
+	EXPECT_TRUE(ABNF::IsIPvFuture("va.z"));
+	EXPECT_TRUE(ABNF::IsIPvFuture("v1.1.1"));
+	EXPECT_TRUE(ABNF::IsIPvFuture("v1.1.:!:$:&:':(:):*:+:,:;:=:-:.:_:~:"));
+}
+
+TEST(uri_abnf, is_ipvfuture_false)
+{
+	EXPECT_FALSE(ABNF::IsIPvFuture(""));
+	EXPECT_FALSE(ABNF::IsIPvFuture("1"));
+	EXPECT_FALSE(ABNF::IsIPvFuture("."));
+	EXPECT_FALSE(ABNF::IsIPvFuture("1."));
+	EXPECT_FALSE(ABNF::IsIPvFuture("v"));
+	EXPECT_FALSE(ABNF::IsIPvFuture("v1"));
+	EXPECT_FALSE(ABNF::IsIPvFuture("v1."));
+	EXPECT_FALSE(ABNF::IsIPvFuture("v.1"));
+	EXPECT_FALSE(ABNF::IsIPvFuture("vz.z"));
+	EXPECT_FALSE(ABNF::IsIPvFuture("v:.z"));
+	EXPECT_FALSE(ABNF::IsIPvFuture("v1.@"));
+	EXPECT_FALSE(ABNF::IsIPvFuture("v1.?"));
+	EXPECT_FALSE(ABNF::IsIPvFuture("v1.\""));
+}
