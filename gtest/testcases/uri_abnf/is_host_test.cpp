5bb06387a5786a7d20f5d1d8ac8ee29ad5c9124f
ywake <ywake@student.42tokyo.jp>
2022-09-04 21:17:39 +0900

    test: 💍 IsHostのテストを追加

--- gtest/testcases/uri_abnf/is_host_test.cpp
+++ gtest/testcases/uri_abnf/is_host_test.cpp
@@ -3,22 +3,30 @@
 #include "parse_authority.hpp"
 
 // host = IP-literal / IPv4address / reg-name
-TEST(uri_abnf, is_host_ipv6)
+TEST(uri_abnf, is_host_ip_literal)
 {
 	EXPECT_TRUE(ABNF::IsHost("[::1]"));
+	EXPECT_TRUE(ABNF::IsHost("[vA.abcdefg]"));
+
+	EXPECT_FALSE(ABNF::IsHost("[127.0.0.1]"));
+	EXPECT_FALSE(ABNF::IsHost("[::1"));
+	EXPECT_FALSE(ABNF::IsHost("::1]"));
+	EXPECT_FALSE(ABNF::IsHost("[vA.abcdefg"));
+	EXPECT_FALSE(ABNF::IsHost("vA.abcdefg]"));
 }
 
 TEST(uri_abnf, is_host_ipv4)
 {
 	EXPECT_TRUE(ABNF::IsHost("127.0.0.1"));
+
+	EXPECT_FALSE(ABNF::IsHost("::1"));
 }
 
-TEST(uri_abnf, is_host_regname_true)
+TEST(uri_abnf, is_host_regname)
 {
 	EXPECT_TRUE(ABNF::IsHost("localhost"));
-}
+	EXPECT_TRUE(ABNF::IsHost("127.0.01"));
 
-TEST(uri_abnf, is_host_regname_false)
-{
+	EXPECT_FALSE(ABNF::IsHost("aiu@eo"));
 	EXPECT_FALSE(ABNF::IsHost("")); // HTTP要件
 }
