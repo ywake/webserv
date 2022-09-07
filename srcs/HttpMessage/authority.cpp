016dac92ecdf2a0545aa4abb986787f8ac13e47d
nori <nori623p@gmail.com>
2022-09-07 19:25:18 +0200

    feat: 🎸 authorityクラスを作った

--- srcs/HttpMessage/authority.cpp
+++ srcs/HttpMessage/authority.cpp
@@ -1,3 +1,29 @@
 #include "authority.hpp"
 
-Authority::Authority(ThinString authority) {}
+#include "error.hpp"
+#include "parse_authority.hpp"
+
+Authority::Authority(ThinString authority)
+{
+	Tokens tokens = TokenizeAuthority(authority);
+	if (!ABNF::IsUserInfo(tokens.userinfo) || !ABNF::IsHost(tokens.host) ||
+		!ABNF::IsPort(tokens.port)) {
+		throw Error("400");
+	}
+	userinfo_ = tokens.userinfo.ToString();
+	host_ = tokens.host.ToString();
+	port_ = tokens.port.ToString();
+}
+
+// TODO ここもうちょい上手く書けるかも
+Authority::Tokens Authority::TokenizeAuthority(ThinString authority)
+{
+	Tokens tokens;
+	ThinString::ThinStrPair userinfo_hostport = authority.DivideBy("@", ThinString::kAlignRight);
+	ThinString hostport = userinfo_hostport.second;
+	ThinString::ThinStrPair host_port = hostport.DivideBy(":");
+	tokens.userinfo = userinfo_hostport.first;
+	tokens.host = host_port.first;
+	tokens.port = host_port.second;
+	return tokens;
+}
--- srcs/HttpMessage/authority.hpp
+++ srcs/HttpMessage/authority.hpp
@@ -6,12 +6,22 @@
 class Authority
 {
   private:
+	typedef struct Tokens {
+		ThinString userinfo;
+		ThinString host;
+		ThinString port;
+	} Tokens;
+
+  private:
 	std::string userinfo_;
 	std::string host_;
 	std::string port_;
 
   public:
 	Authority(ThinString authority);
+
+  private:
+	Tokens TokenizeAuthority(ThinString authority);
 };
 
 #endif // AUTHORITY_HPP
