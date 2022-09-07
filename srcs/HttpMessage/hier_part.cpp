b4289447f64f26c690389517276a474f1c3e81d7
nori <nori623p@gmail.com>
2022-09-07 19:25:04 +0200

    fix: 🐛 一時的にコンパイルできるように修正

--- srcs/HttpMessage/hier_part.cpp
+++ srcs/HttpMessage/hier_part.cpp
@@ -1,3 +1,6 @@
 #include "hier_part.hpp"
 
-HierPart::HierPart(ThinString hier_part) {}
+HierPart::HierPart(ThinString hier_part) : authority_("a")
+{
+	(void)hier_part;
+}
--- srcs/HttpMessage/http_uri.cpp
+++ srcs/HttpMessage/http_uri.cpp
@@ -2,9 +2,17 @@
 
 HttpUri::HttpUri() : Uri() {}
 
-void HttpUri::ParseOriginForm(ThinString uri) {
+void HttpUri::ParseOriginForm(ThinString uri)
+{
+	(void)uri;
 }
 
-void HttpUri::ParseAbsoluteForm(ThinString uri) {}
+void HttpUri::ParseAbsoluteForm(ThinString uri)
+{
+	(void)uri;
+}
 
-void HttpUri::ParseAuthorityForm(ThinString uri) {}
+void HttpUri::ParseAuthorityForm(ThinString uri)
+{
+	(void)uri;
+}
