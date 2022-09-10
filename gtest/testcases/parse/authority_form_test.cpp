95a64acf1892d54a5a4bb87824defc35546f91d1
yaito6502 <schwarzekatze8010110@gmail.com>
2022-09-10 17:59:14 +0900

    test: 💍 AuthortyFormのテストを追加

---
+++ gtest/testcases/parse/authority_form_test.cpp
@@ -0,0 +1,24 @@
+#include "authority_form.hpp"
+#include "error.hpp"
+#include "gtest.h"
+
+// PORTがABNFに適合しないなど、AuthorityFormのメンバに対してのテストは行っていない。(authority_test.cppに既にあるため)
+//ここではHTTP要件によってvalidになったり、invalidになったりするケースをテストする。
+
+TEST(AuthorityFormTest, valid_authority_form)
+{
+	EXPECT_EQ(AuthorityForm("example.com:80"), AuthorityForm("example.com:80"));
+}
+
+TEST(AuthorityFormTest, invalid_on_http)
+{
+	EXPECT_THROW(AuthorityForm(""), Error);
+	EXPECT_THROW(AuthorityForm(":"), Error);
+	EXPECT_THROW(AuthorityForm(":80"), Error);
+}
+
+TEST(AuthorityFormTest, invalid_authority_form)
+{
+	EXPECT_THROW(AuthorityForm("example.com"), Error);
+	EXPECT_THROW(AuthorityForm("example.com80"), Error);
+}
