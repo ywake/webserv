4b9eb0ef98390c0e48dc2460ac850fafc1c6cad5
nori <nori623p@gmail.com>
2022-09-10 08:41:11 +0200

    refactor: 💡 form_type_いらないかもと思った　消した

--- srcs/HttpMessage/request_target.hpp
+++ srcs/HttpMessage/request_target.hpp
@@ -10,39 +10,18 @@
 
 class RequestTarget
 {
-  public:
-	enum RequestForm {
-		ORIGIN,
-		ABSOLUTE,
-		AUTHORITY,
-		ASTERISK,
-		UNDEFINED
-	};
-
   private:
-	RequestForm form_type_;
-	/**
-	 * Asterisk-formのみURI形式にパースできないので、
-	 * Asterisk-formはform_typeで特定する。
-	 */
 	RequestFormData form_data_;
 
   public:
 	RequestTarget();
-	RequestTarget(const OriginForm &form);
-	RequestTarget(const AbsoluteForm &form);
-	RequestTarget(const AuthorityForm &form);
-	RequestTarget(const AsteriskForm &form);
+	RequestTarget(const ITargetForm &form);
 	RequestTarget(std::string uri);
-	RequestTarget(RequestForm form_type, RequestFormData request_target);
+	RequestTarget(RequestFormData request_target);
 
-	RequestTarget			  &operator=(const RequestTarget &other);
-	bool					   operator==(const RequestTarget &rhs) const;
-	bool					   operator!=(const RequestTarget &rhs) const;
-	RequestTarget::RequestForm SpecifyForm(const std::string &uri);
-	void					   SetFormData(const ITargetForm *uri);
-
-	RequestForm GetRequestForm() const;
+	RequestTarget &operator=(const RequestTarget &other);
+	bool		   operator==(const RequestTarget &rhs) const;
+	bool		   operator!=(const RequestTarget &rhs) const;
 };
 
 std::ostream &operator<<(std::ostream &os, const RequestTarget &request_target);
