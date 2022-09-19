11c15c4ce0415b0dcbf13a3d95f51cacadf2d065
nori <nori623p@gmail.com>
2022-09-19 22:07:19 +0200

    feat: 🎸 WIP ちょっとコンストラクタを書いた

--- srcs/http_message/field_line.cpp
+++ srcs/http_message/field_line.cpp
@@ -1,9 +1,20 @@
 #include "field_line.hpp"
+#include "error.hpp"
 #include "http_abnf_space.hpp"
 
 static const std::string kCrLf = "\r\n";
 
 FieldLine::FieldLine() : field_name_(), field_value_() {}
+FieldLine::FieldLine(const ThinString &line)
+{
+	std::size_t colon_pos = line.find(":");
+	if (colon_pos == ThinString::npos) {
+		throw Error("400");
+	}
+	ThinString name  = line.substr(0, colon_pos);
+	ThinString value = line.substr(colon_pos + 1);
+}
+
 const ThinString &FieldLine::GetFieldName() const
 {
 	return field_value_;
