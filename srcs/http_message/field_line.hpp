ca564c166de92387a697b7af0b2c867160e08cfd
nori <nori623p@gmail.com>
2022-09-19 07:59:07 +0200

    docs: ✏️ obs-foldのbnfを修正

--- srcs/http_message/field_line.hpp
+++ srcs/http_message/field_line.hpp
@@ -50,17 +50,17 @@ class FieldLines
 };
 // TODO BNFミスってる
 /*
-  field-lines   = *( one-line )
-  one-line      = *(field-line obs-fold ) field-line CRLF
   field-line    = field-name ":" OWS field-value OWS
 
   field-name    = token
 
-  field-value   = *field-content
+  field-value   = *(field-content / obs-fold)
   field-content = field-vchar [ 1*( SP / HTAB / field-vchar ) field-vchar ]
   field-vchar   = VCHAR / obs-text
   obs-text      = %x80-FF
+
   obs-fold      = OWS CRLF RWS
+  ; obs-foldのみの連続も可能で、SP一つと同等に扱う RFC2616 2.2
 
 */
 
