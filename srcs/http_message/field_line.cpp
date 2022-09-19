5b91945b2dab9d7527e16b53a6fb2c96a7316084
nori <nori623p@gmail.com>
2022-09-19 18:23:46 +0200

    feat: 🎸 fieldlineクラスを追加

---
+++ srcs/http_message/field_line.cpp
@@ -0,0 +1,4 @@
+#include "field_line.hpp"
+
+FieldLine::FieldLine() : field_name_(), field_value_() {}
+FieldLine::FieldLine(const ThinString &line) {}
---
+++ srcs/http_message/field_line.hpp
@@ -0,0 +1,32 @@
+#ifndef FIELD_LINE_HPP
+#define FIELD_LINE_HPP
+
+#include "thin_string.hpp"
+
+class FieldLine
+{
+  private:
+	ThinString field_name_;
+	ThinString field_value_;
+
+  public:
+	FieldLine();
+	FieldLine(const ThinString &line);
+};
+
+/*
+  field-line    = field-name ":" OWS field-value OWS
+
+  field-name    = token
+
+  field-value   = *(field-content / obs-fold)
+  field-content = field-vchar [ 1*( SP / HTAB / field-vchar ) field-vchar ]
+  field-vchar   = VCHAR / obs-text
+  obs-text      = %x80-FF
+
+  obs-fold      = OWS CRLF RWS
+  ; obs-foldのみの連続も可能で、SP一つと同等に扱う RFC2616 2.2
+
+*/
+
+#endif
