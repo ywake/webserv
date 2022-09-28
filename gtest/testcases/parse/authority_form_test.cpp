479eabbd082151409155a4df933ab432972f8562
yaito6502 <schwarzekatze8010110@gmail.com>
2022-09-28 17:53:19 +0900

    test: 💍 テスト名が一部スネークケースになっていなかったので修正

--- gtest/testcases/parse/authority_form_test.cpp
+++ gtest/testcases/parse/authority_form_test.cpp
@@ -5,7 +5,7 @@
 // PORTがABNFに適合しないなど、AuthorityFormのメンバに対してのテストは行っていない。(authority_test.cppに既にあるため)
 //ここではHTTP要件によってvalidになったり、invalidになったりするケースをテストする。
 
-TEST(AuthorityFormTest, valid_authority_form)
+TEST(authority_form_test, valid_authority_form)
 {
 	EXPECT_EQ(
 		AuthorityForm("example.com:80"), AuthorityForm(http_abnf::HostPort("example.com:80"))
@@ -13,14 +13,14 @@ TEST(AuthorityFormTest, valid_authority_form)
 	EXPECT_EQ(AuthorityForm("example.com:"), AuthorityForm(http_abnf::HostPort("example.com:")));
 }
 
-TEST(AuthorityFormTest, invalid_on_http)
+TEST(authority_form_test, invalid_on_http)
 {
 	EXPECT_THROW(AuthorityForm(""), Error);
 	EXPECT_THROW(AuthorityForm(":"), Error);
 	EXPECT_THROW(AuthorityForm(":80"), Error);
 }
 
-TEST(AuthorityFormTest, invalid_authority_form)
+TEST(authority_form_test, invalid_authority_form)
 {
 	EXPECT_THROW(AuthorityForm("example.com"), Error);
 	EXPECT_THROW(AuthorityForm("example.com80"), Error);
