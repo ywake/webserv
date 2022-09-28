cf8d12cad3e1091b2eb9c2dad1d55e8743f6e5a3
nori <nori623p@gmail.com>
2022-09-28 10:21:24 +0200

    feat: 🎸 hostportクラスを追加

---
+++ srcs/http_message/http_abnf/host_port.hpp
@@ -0,0 +1,31 @@
+#ifndef HOST_PORT_HPP
+#define HOST_PORT_HPP
+
+#include "thin_string.hpp"
+
+namespace http_abnf
+{
+	class HostPort
+	{
+	  private:
+		ThinString host_;
+		ThinString port_;
+
+	  public:
+		HostPort();
+		HostPort(const ThinString &host, const ThinString &port);
+		HostPort(const ThinString &str);
+		HostPort(const HostPort &other);
+
+		const ThinString &GetHost() const;
+		const ThinString &GetPort() const;
+
+		HostPort &operator=(const HostPort &other);
+		bool      operator==(const HostPort &other) const;
+		bool      operator!=(const HostPort &other) const;
+	};
+} // namespace http_abnf
+
+std::ostream &operator<<(std::ostream &os, const http_abnf::HostPort &host_port);
+
+#endif
