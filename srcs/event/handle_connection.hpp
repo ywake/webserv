6cdb4390f9bea6dd20e816e16d121976a8e66b0d
nori <nori623p@gmail.com>
2022-10-12 00:25:03 +0200

    fix: 🐛 pollクラス名の変更を適用

--- srcs/event/handle_connection.hpp
+++ srcs/event/handle_connection.hpp
@@ -1,22 +1,22 @@
-#ifndef HANDLE_CONNECTION_EVENT_HPP
-#define HANDLE_CONNECTION_EVENT_HPP
+#ifndef HANDLE_CONNECTION_HPP
+#define HANDLE_CONNECTION_HPP
 
 #include "i_event.hpp"
-#include "io_multiplexer.hpp"
+#include "poll.hpp"
 #include "server_types.hpp"
 namespace event
 {
 	class HandleConnection : public IEvent
 	{
 	  private:
-		IoMultiplexer       &io_monitor_;
-		server::Connections &connections_;
+		io_multiplexer::Poll &io_monitor_;
+		server::Connections  &connections_;
 
 	  private:
 		HandleConnection();
 
 	  public:
-		HandleConnection(IoMultiplexer &io_monitor, server::Connections &connections);
+		HandleConnection(io_multiplexer::Poll &io_monitor, server::Connections &connections);
 		void Run();
 	};
 } // namespace event
