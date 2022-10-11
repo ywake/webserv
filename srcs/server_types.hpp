e6b3bcf32107f4222692d071c8fc106fde55ab31
nori <nori623p@gmail.com>
2022-10-11 23:49:02 +0200

    feat: 🎸 pollInstructionのcontainerをtypedef

--- srcs/server_types.hpp
+++ srcs/server_types.hpp
@@ -1,15 +1,18 @@
 #ifndef SERVER_TYPES_HPP
 #define SERVER_TYPES_HPP
 
+#include <list>
 #include <set>
 
 #include "connection.hpp"
+#include "poll_instruction.hpp"
 #include "virtual_servers.hpp"
 
 namespace server
 {
-	typedef std::set<Connection> Connections;
-	typedef conf::VirtualServers Configs;
+	typedef std::set<Connection>                       Connections;
+	typedef conf::VirtualServers                       Configs;
+	typedef std::list<io_multiplexer::PollInstruction> PollInstructions;
 } // namespace server
 
 #endif
