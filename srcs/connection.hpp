de6268c71c6e514635abeca644bba8ab82755cbf
nori <nori623p@gmail.com>
2022-10-11 22:36:12 +0200

    feat: 🎸 wip connectionクラスの定義修正

--- srcs/connection.hpp
+++ srcs/connection.hpp
@@ -1,11 +1,44 @@
 #ifndef CONNECTION_HPP
 #define CONNECTION_HPP
 
-// TODO fd class
-class Connection
-{
-  private:
-	int fd_;
-};
+#include <netinet/in.h>
 
+#include "poll_instruction.hpp"
+#include "receiver.hpp"
+#include "sender.hpp"
+#include "server_conf.hpp"
+#include "server_types.hpp"
+
+// TODO fd
+namespace server
+{
+	class Connection
+	{
+	  public:
+		enum State {
+			kRecv,
+			kSend,
+		};
+
+	  private:
+		typedef struct sockaddr_in SockAddrIn;
+
+	  private:
+		int                     fd_;
+		const conf::ServerConf &configs_;
+		const SockAddrIn        client_;
+		State                   state_;
+		Receiver                receiver_;
+		Sender				 *sender;
+
+	  public:
+		Connection(int fd, const conf::ServerConf &conf, const SockAddrIn &client);
+		State                           GetState();
+		io_multiplexer::PollInstruction Proceed();
+
+	  private:
+		void                            Receive();
+		io_multiplexer::PollInstruction Send();
+	};
+} // namespace server
 #endif
