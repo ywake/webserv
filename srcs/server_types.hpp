661a8e9d7da76620073613e5a351fd976263adac
nori <nori623p@gmail.com>
2022-10-12 00:19:00 +0200

    feat: 🎸 listenerのコンテナtypedefを追加

--- srcs/server_types.hpp
+++ srcs/server_types.hpp
@@ -5,11 +5,12 @@
 #include <set>
 
 #include "connection.hpp"
+#include "listener.hpp"
 #include "poll_instruction.hpp"
 #include "virtual_servers.hpp"
-
 namespace server
 {
+	typedef std::vector<Listener>                      Listeners;
 	typedef std::set<Connection>                       Connections;
 	typedef conf::VirtualServers                       Configs;
 	typedef std::list<io_multiplexer::PollInstruction> PollInstructions;
