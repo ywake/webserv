5e467e7bb439160ce58fcdb0cec4cea07edb28e0
nori <nori623p@gmail.com>
2022-10-11 22:33:41 +0200

    feat: 🎸 wip listenerクラスがわだけ

--- srcs/listener.hpp
+++ srcs/listener.hpp
@@ -1,15 +1,20 @@
 #ifndef LISTENER_HPP
 #define LISTENER_HPP
 
+#include "server_conf.hpp"
 // TODO fdclass
-class Listener
+namespace server
 {
-  private:
-	int fd_;
+	class Listener
+	{
+	  private:
+		int               fd_;
+		conf::ServerConf &configs_;
 
-  public:
-	Listener(int fd);
-	int Accept();
-};
-
+	  public:
+		Listener(int fd);
+		int Accept();
+		int Listen();
+	};
+} // namespace server
 #endif
