d2e80e97f62a1263c798d535efab8435219cda1f
nori <nori623p@gmail.com>
2022-10-12 08:05:41 +0200

    feat: 🎸 wip senderのコンストラクタの宣言を追加

--- srcs/sender.hpp
+++ srcs/sender.hpp
@@ -2,6 +2,7 @@
 #define SENDER_HPP
 
 #include "poll_instruction.hpp"
+#include "request_message.hpp"
 #include "virtual_server_conf.hpp"
 
 namespace server
@@ -18,10 +19,22 @@ namespace server
 	  private:
 		int                      fd_;
 		State                    state_;
+		http::RequestMessage     request_; // sender 側で変更し得るので非const
 		conf::VirtualServerConf &config_;
+		const SockAddrIn        &client_;
+
+	  private:
+		Sender();
 
 	  public:
+		Sender(
+			int                            fd,
+			const conf::VirtualServerConf &config,
+			const http::RequestMessage    &request,
+			const SockAddrIn              &client
+		);
 		io_multiplexer::PollInstruction Proceed();
 	};
 } // namespace server
+
 #endif
