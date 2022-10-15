697a425fd29279b3f7e927e390b2b8faf86878b4
nori <nori623p@gmail.com>
2022-10-15 19:13:38 +0200

    feat: 🎸 poll_instructionとりあえずできたかも

--- srcs/poll_instruction.hpp
+++ srcs/poll_instruction.hpp
@@ -2,12 +2,24 @@
 #define POLL_INSTRUCTION_HPP
 
 #include <list>
+#include <sys/epoll.h>
 
 namespace io_multiplexer
 {
+	typedef struct PollInstruction                     PollInstruction;
 	typedef std::list<io_multiplexer::PollInstruction> PollInstructions;
-	class PollInstruction
-	{
+
+	struct PollInstruction {
+	  public:
+		enum Instruction {
+			kRegister,
+			kUnregister,
+		};
+
+	  public:
+		int         fd;
+		uint32_t    event;
+		Instruction instruction;
 	};
 } // namespace io_multiplexer
 
