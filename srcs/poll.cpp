c8924ad0987ecc4619b85111afff3a7a2ebdb0f2
nori <nori623p@gmail.com>
2022-10-17 14:30:39 +0200

    feat: 🎸 pollクラス一応それっぽくなった

--- srcs/poll.cpp
+++ srcs/poll.cpp
@@ -2,12 +2,11 @@
 
 #include <cerrno>
 #include <cstring>
-#include <stdexcept>
 #include <sys/epoll.h>
 
 namespace io_multiplexer
 {
-	Poll::Poll(int timeout) : timeout_(timeout), maxevents_()
+	Poll::Poll(int timeout) : timeout_(timeout), registered_events_()
 	{
 		epoll_fd_ = epoll_create1(EPOLL_CLOEXEC);
 		if (epoll_fd_ == -1) {
@@ -17,12 +16,13 @@ namespace io_multiplexer
 
 	Result<PollEvents> Poll::Wait()
 	{
-		PollEvents          events(maxevents_);
+		int                 num_of_events = registered_events_.size();
+		PollEvents          events(num_of_events);
 		struct epoll_event *events_ptr  = events.data();
 		int                 events_size = 0;
 
 		while (true) {
-			events_size = epoll_wait(epoll_fd_, events_ptr, maxevents_, timeout_);
+			events_size = epoll_wait(epoll_fd_, events_ptr, num_of_events, timeout_);
 			if (events_size != -1) {
 				break;
 			} else if (errno == EINTR)
@@ -35,5 +35,106 @@ namespace io_multiplexer
 		return events;
 	}
 
+	ErrEvents Poll::InstructMulti(const PollInstructions &instructions)
+	{
+		ErrEvents err_events;
+
+		for (PollInstructions::const_iterator it = instructions.begin(); it != instructions.end();
+			 it++) {
+			Result<void> res = Instruct(*it);
+			if (res.IsErr()) {
+				err_events.push_back((*it, Error(res.Err())));
+			}
+		}
+		return err_events;
+	}
+
+	Result<void> Poll::Instruct(const PollInstruction &instruction)
+	{
+		switch (instruction.command) {
+		case PollInstruction::kAppendEventType:
+			return AppendEventType(instruction);
+		case PollInstruction::kTrimEventType:
+			return TrimEventType(instruction);
+		case PollInstruction::kRegister:
+			return Register(instruction.fd, instruction.event);
+		case PollInstruction::kUnregister:
+			return Unregister(instruction.fd);
+		}
+		return Result<void>();
+	}
+
+	Result<void> Poll::AppendEventType(const PollInstruction &instruction)
+	{
+		Result<void>       res;
+		struct epoll_event event = {};
+
+		try {
+			event = registered_events_.at(instruction.fd);
+		} catch (const std::out_of_range &e) {
+			(void)e;
+		}
+		// TODO operator=
+		res = Unregister(instruction.fd);
+		if (res.IsErr()) {
+			return res;
+		}
+		event.events |= instruction.event.events;
+		event.data = instruction.event.data;
+		res        = Register(instruction.fd, event);
+		if (res.IsErr()) {
+			return res;
+		}
+		return Result<void>();
+	}
+
+	Result<void> Poll::TrimEventType(const PollInstruction &instruction)
+	{
+		Result<void>       res;
+		struct epoll_event event = {};
+
+		try {
+			event = registered_events_.at(instruction.fd);
+		} catch (const std::out_of_range &e) {
+			(void)e;
+		}
+		res = Unregister(instruction.fd);
+		if (res.IsErr()) {
+			return res;
+		}
+		event.events &= ~instruction.event.events;
+		if (!event.events) {
+			return Result<void>();
+		}
+		res = Register(instruction.fd, event);
+		if (res.IsErr()) {
+			return res;
+		}
+		return Result<void>();
+	}
+
+	Result<void> Poll::Register(int fd, const struct epoll_event &event)
+	{
+		struct epoll_event ev = event;
+		if (epoll_ctl(epoll_fd_, EPOLL_CTL_ADD, fd, &ev) == -1) {
+			return Error(std::string("epoll append2: ") + strerror(errno));
+		}
+		registered_events_[fd] = event;
+		return Result<void>();
+	}
+
+	Result<void> Poll::Unregister(int fd)
+	{
+		struct epoll_event unused;
+
+		if (epoll_ctl(epoll_fd_, EPOLL_CTL_DEL, fd, &unused) == -1) {
+			if (errno == ENOENT) {
+				return Result<void>();
+			}
+			return Error(std::string("epoll append1: ") + strerror(errno));
+		}
+		registered_events_.erase(fd);
+		return Result<void>();
+	}
 
 } // namespace io_multiplexer
--- srcs/poll.hpp
+++ srcs/poll.hpp
@@ -1,6 +1,8 @@
 #ifndef POLL_HPP
 #define POLL_HPP
 
+#include <list>
+#include <map>
 #include <vector>
 
 #include "poll_instruction.hpp"
@@ -8,18 +10,25 @@
 
 namespace io_multiplexer
 {
-	typedef std::vector<struct epoll_event> PollEvents;
+	typedef std::vector<struct epoll_event>           PollEvents;
+	typedef std::list<Result<const PollInstruction> > ErrEvents;
 
 	class Poll
 	{
 	  private:
-		int epoll_fd_;
-		int timeout_;
-		int maxevents_;
+		int                               epoll_fd_;
+		int                               timeout_;
+		std::map<int, struct epoll_event> registered_events_;
 
 	  public:
 		Poll(int timeout = -1);
 		Result<PollEvents> Wait();
+		ErrEvents          InstructMulti(const PollInstructions &instructions);
+		Result<void>       Instruct(const PollInstruction &instruction);
+		Result<void>       AppendEventType(const PollInstruction &instruction);
+		Result<void>       TrimEventType(const PollInstruction &instruction);
+		Result<void>       Register(int fd, const struct epoll_event &event);
+		Result<void>       Unregister(int fd);
 	};
 } // namespace io_multiplexer
 
