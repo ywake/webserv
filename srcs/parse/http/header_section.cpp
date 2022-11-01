c6cdbe769271e514f8285bc90397d6e494650316
yaito6502 <schwarzekatze8010110@gmail.com>
2022-11-02 06:51:57 +0900

    fix: 🐛 GetKeyValueStringにて、存在しないkeyで呼ばれた時に例外を投げてしまう問題を修正

--- srcs/parse/http/header_section.cpp
+++ srcs/parse/http/header_section.cpp
@@ -267,6 +267,9 @@ std::string HeaderSection::GetKeyValueString(const std::string &key) const
 	std::string value     = "";
 	std::string lower_key = utils::ToLowerString(key);
 
+	if (!Contains(lower_key)) {
+		return lower_key + "=";
+	}
 	HeaderSection::Values values = field_lines_.at(lower_key);
 	for (HeaderSection::Values::iterator it = values.begin(); it != values.end(); it++) {
 		value += it->GetValue();
