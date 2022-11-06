f9621fbc27b592c911d58ee8527ce18d3d343646
nori <nori623p@gmail.com>
2022-11-06 16:34:38 +0100

    feat: 🎸 bufferクラスを実装

---
+++ srcs/buffer/buffer.cpp
@@ -0,0 +1,73 @@
+#include "buffer.hpp"
+
+namespace buffer
+{
+	Buffer::Buffer(std::size_t max_inner_buf_size)
+		: inner_buf_(), idx_(), max_inner_buf_size_(max_inner_buf_size)
+	{}
+
+	Buffer::Buffer(const Buffer &other)
+	{
+		*this = other;
+	}
+
+	Result<void> Buffer::push_back(const std::vector<char> &data)
+	{
+		if (IsFull()) {
+			return Error("buffer is full");
+		}
+		if (data.empty()) {
+			return Result<void>();
+		}
+		inner_buf_.push_back(data);
+		return Result<void>();
+	}
+
+	Result<char> Buffer::GetChar()
+	{
+		if (empty()) {
+			return Error("buffer is empty");
+		}
+		ByteArray &bytes = inner_buf_.front();
+		char       c     = bytes.at(idx_); // 例外は起きないはず
+		idx_++;
+		if (idx_ == bytes.size()) {
+			idx_ = 0;
+			inner_buf_.pop_front();
+		}
+		return c;
+	}
+
+	std::vector<char> Buffer::GetAll()
+	{
+		ByteArray all;
+
+		for (; !inner_buf_.empty();) {
+			ByteArray &buf = inner_buf_.front();
+			std::copy(buf.begin(), buf.end(), std::back_inserter(all));
+			inner_buf_.pop_front();
+		}
+		return all;
+	}
+
+	bool Buffer::empty() const
+	{
+		return inner_buf_.empty();
+	}
+
+	bool Buffer::IsFull() const
+	{
+		return inner_buf_.size() == max_inner_buf_size_;
+	}
+
+	Buffer &Buffer::operator=(const Buffer &other)
+	{
+		if (this == &other) {
+			return *this;
+		}
+		inner_buf_          = other.inner_buf_;
+		idx_                = other.idx_;
+		max_inner_buf_size_ = other.max_inner_buf_size_;
+		return *this;
+	}
+} // namespace buffer
---
+++ srcs/buffer/buffer.hpp
@@ -0,0 +1,37 @@
+#ifndef BUFFER_HPP
+#define BUFFER_HPP
+
+#include <deque>
+#include <vector>
+
+#include "result.hpp"
+
+// TODO rename
+namespace buffer
+{
+	// TODO rename
+	class Buffer
+	{
+	  private:
+		typedef std::vector<char>              ByteArray;
+		typedef std::deque<std::vector<char> > InnerBuffer;
+
+	  private:
+		static const std::size_t kDefaultMaxSize = 10;
+		InnerBuffer              inner_buf_;
+		std::size_t              idx_;
+		std::size_t              max_inner_buf_size_;
+
+	  public:
+		Buffer(std::size_t max_inner_buf_size = kDefaultMaxSize);
+		Buffer(const Buffer &other);
+		Result<void>      push_back(const std::vector<char> &data);
+		Result<char>      GetChar();
+		std::vector<char> GetAll();
+		bool              empty() const;
+		bool              IsFull() const;
+		Buffer           &operator=(const Buffer &other);
+	};
+} // namespace buffer
+
+#endif
