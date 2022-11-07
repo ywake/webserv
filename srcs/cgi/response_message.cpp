508d41a8c434a76aa3d95ee66ea68c4393b4338f
yaito6502 <schwarzekatze8010110@gmail.com>
2022-11-08 02:10:39 +0900

    fix: 🐛 不要になったクラス削除

--- srcs/cgi/response_message.cpp
+++
@@ -1,8 +0,0 @@
-#include "response_message.hpp"
-#include <cerrno>
-#include <cstring>
-#include <stdexcept>
-
-using cgi::ResponseMessage;
-
-ResponseMessage::ResponseMessage() : header_fields_(), message_body_() {}
