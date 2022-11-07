6d47228c67b60f8e03939940043351707c1e40f7
nori <nori623p@gmail.com>
2022-11-07 20:44:49 +0100

    feat: 🎸 eventtypeをbitで持てる世に判定

--- srcs/event/event.hpp
+++ srcs/event/event.hpp
@@ -1,16 +1,19 @@
 #ifndef EVENT_HPP
 #define EVENT_HPP
 
+#include <stdint.h>
+
 namespace event
 {
 	typedef struct Event {
 		enum EventType {
-			kWrite,
-			kRead,
+			kWrite = 1U << 0,
+			kRead  = 1U << 1,
 		};
-		int       fd;
-		void     *event;
-		EventType event_type;
+
+		int      fd;
+		uint32_t event_type;
+		void    *data;
 	} Event;
 } // namespace event
 
