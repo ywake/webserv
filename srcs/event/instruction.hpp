066a7b49324d0e861e3c0e0d7ce2b42ae3322f07
nori <nori623p@gmail.com>
2022-11-07 20:43:53 +0100

    feat: 🎸 list<instruction>のtypedefを追加

--- srcs/event/instruction.hpp
+++ srcs/event/instruction.hpp
@@ -1,9 +1,12 @@
 #ifndef INSTRUCTION_HPP
 #define INSTRUCTION_HPP
 
+#include <list>
+
 #include "event.hpp"
 namespace event
 {
+	typedef std::list<Instruction> Instructions;
 	typedef struct Instruction {
 		enum Command {
 			kAppendEventType,
