abfbfaecc0fc2d9b17bad8bfff89d69bd051a0a8
nori <nori623p@gmail.com>
2022-11-18 17:10:17 +0100

    feat: 🎸 server-classのコンストラクタを実装

--- srcs/server/server.cpp
+++ srcs/server/server.cpp
@@ -5,8 +5,13 @@ namespace server
 {
 	Server::Server(const conf::ServerConfs &configs) : configs_(configs), io_monitor_(kEpollTimeout)
 	{
-		// lisnters 作成
-		//  for (configs_)
+		typedef conf::ServerConfs::ConfsMap::const_iterator ConfsItr;
+		for (ConfsItr it = configs_.begin(); it != configs_.end(); it++) {
+			const std::string              &port     = it->first;
+			const conf::VirtualServerConfs &conf     = it->second;
+			Listener                        listener = Listener(port, conf);
+			listeners_.push_back(listener);
+		}
 	}
 
 } // namespace server
