16211328afebd54d1f81d9b07eb4f34b4dc6202a
ywake <ywake@student.42tokyo.jp>
2022-11-19 16:50:07 +0900

    fix: 🐛 overflowの計算式を修正

--- srcs/config/server_conf.cpp
+++ srcs/config/server_conf.cpp
@@ -141,12 +141,9 @@ namespace conf
 		}
 		std::size_t unit_size = unit.Val();
 
-		if (size > std::numeric_limits<std::size_t>::max() / unit_size) {
-			throw ConfigException("Invalid client_max_body_size");
-		}
-
 		client_max_body_size_ = size * unit_size;
-		if (client_max_body_size_ == 0 || client_max_body_size_.Value() > kMaxClientBodySize) {
+		if (size != client_max_body_size_.Value() / unit_size || client_max_body_size_ == 0 ||
+			client_max_body_size_.Value() > kMaxClientBodySize) {
 			throw ConfigException("Invalid client_max_body_size");
 		}
 	}
