5f95b18ab58de3fc4d58ce5cac6f541d4638c312
nori <nori623p@gmail.com>
2022-11-20 05:09:13 +0100

    refactor: 💡 using namespace eventすることにしたかも

--- srcs/server/server.cpp
+++ srcs/server/server.cpp
@@ -4,6 +4,8 @@
 
 namespace server
 {
+	using namespace event;
+
 	Server::Server(const conf::ServerConfs &configs) : configs_(configs), io_monitor_(kEpollTimeout)
 	{
 		typedef conf::ServerConfs::ConfsMap::const_iterator ConfsItr;
@@ -22,10 +24,10 @@ namespace server
 			if (listen_result.IsErr()) {
 				return Error(listen_result.Err());
 			}
-			uint32_t           event_type     = event::Event::kWrite | event::Event::kRead;
-			event::Event       ev             = {it->GetFd(), &*it, event_type};
-			event::Instruction instruction    = {event::Instruction::kRegister, ev};
-			Result<void>       intruct_result = io_monitor_.Instruct(instruction);
+			uint32_t     event_type     = Event::kWrite | Event::kRead;
+			Event        ev             = {it->GetFd(), &*it, event_type};
+			Instruction  instruction    = {Instruction::kRegister, ev};
+			Result<void> intruct_result = io_monitor_.Instruct(instruction);
 			if (intruct_result.IsErr()) {
 				Error(intruct_result.Err());
 			}
@@ -36,12 +38,12 @@ namespace server
 	void Server::Run()
 	{
 		while (true) {
-			Result<event::Events> events = io_monitor_.Wait();
+			Result<Events> events = io_monitor_.Wait();
 			if (events.IsErr()) {
 				log(events.Err());
 			}
-			event::Instructions instructions            = RunEvents(events.Val());
-			event::Instructions unregister_instructions = CloseFinishedConnections();
+			Instructions instructions            = RunEvents(events.Val());
+			Instructions unregister_instructions = CloseFinishedConnections();
 			instructions.splice(instructions.end(), unregister_instructions);
 			io_multiplexer::ErrEvents err = io_monitor_.InstructMulti(instructions);
 			if (!err.empty()) {
@@ -50,15 +52,15 @@ namespace server
 		}
 	}
 
-	event::Instructions Server::RunEvents(const event::Events &events)
+	Instructions Server::RunEvents(const Events &events)
 	{
 		(void)events;
-		return event::Instructions();
+		return Instructions();
 	}
 
-	event::Instructions Server::CloseFinishedConnections()
+	Instructions Server::CloseFinishedConnections()
 	{
-		return event::Instructions();
+		return Instructions();
 	}
 
 } // namespace server
