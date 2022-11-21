1680c0fb6956f01dac38a1b333c6b3cca013c9ce
ywake <ywake@student.42tokyo.jp>
2022-11-21 19:58:24 +0900

    test: 💍 testの修正

--- gtest/testcases/config/defalut_value_test.cpp
+++ gtest/testcases/config/defalut_value_test.cpp
@@ -9,7 +9,7 @@ TEST(config, default_server_conf)
 	EXPECT_EQ(server_conf.GetListenPort(), conf::ServerConf::ListenPort({"80"}));
 	EXPECT_EQ(server_conf.GetServerName(), conf::ServerConf::ServerName());
 	EXPECT_EQ(server_conf.GetErrorPages(), conf::ServerConf::ErrorPages());
-	EXPECT_EQ(server_conf.GetClientMaxBodySize(), conf::ServerConf::ClientMaxBodySize(1 << 20));
+	EXPECT_EQ(server_conf.GetClientMaxBodySize(), 1 << 20);
 	EXPECT_EQ(server_conf.GetLocationConfs(), conf::ServerConf::LocationConfs());
 }
 
@@ -23,6 +23,6 @@ TEST(config, default_location_conf)
 	EXPECT_EQ(location_conf.GetRedirect(), conf::LocationConf::Redirect());
 	EXPECT_EQ(location_conf.GetRoot(), conf::LocationConf::Root());
 	EXPECT_EQ(location_conf.GetIndexFiles(), conf::LocationConf::IndexFiles({"index.html"}));
-	EXPECT_EQ(location_conf.GetAutoindex(), conf::LocationConf::AutoIndex(false));
+	EXPECT_EQ(location_conf.GetAutoindex(), false);
 	EXPECT_EQ(location_conf.GetCgiPath(), conf::LocationConf::CgiPath());
 }
