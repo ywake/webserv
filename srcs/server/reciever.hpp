10f563b4706a152298e934fac7b8a29418c7bdea
nori <nori623p@gmail.com>
2022-11-22 18:24:24 +0100

    fix: 🐛 デフォルトのバッファサイズを定数で定義

--- srcs/server/reciever.hpp
+++ srcs/server/reciever.hpp
@@ -10,12 +10,15 @@ namespace server
 	class Reciever : public buffer::Buffer
 	{
 	  private:
+		static const std::size_t kDefaultBufferSize;
+
+	  private:
 		int    fd_;
 		size_t buffer_size_;
 		bool   is_eof_;
 
 	  public:
-		Reciever(int fd = -1, std::size_t buffer_size = 1024);
+		Reciever(int fd = -1, std::size_t buffer_size = kDefaultBufferSize);
 		Result<void> Recv();
 		bool         IsEof();
 	};
