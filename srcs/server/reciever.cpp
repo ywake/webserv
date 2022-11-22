5c2014dad3a8088947347d570ada40e5ac72fc53
nori <nori623p@gmail.com>
2022-11-22 17:49:13 +0100

    feat: 🎸 reciever()を実装

---
+++ srcs/server/reciever.cpp
@@ -0,0 +1,15 @@
+#include <cerrno>
+#include <cstdio>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include "reciever.hpp"
+
+namespace server
+{
+	Reciever::Reciever(int fd, std::size_t buffer_size)
+		: fd_(fd), buffer_size_(buffer_size), is_eof_(false)
+	{}
+
+
+} // namespace server
