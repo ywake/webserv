661c3a0559dbcef3edde515073a50f1b55cf1eea
nori <nori623p@gmail.com>
2022-11-23 15:26:19 +0100

    fix: 🐛 HttMessageをcppに分離

---
+++ srcs/parse/http/http_message.cpp
@@ -0,0 +1,13 @@
+#include "http_message.hpp"
+
+namespace http
+{
+	HttpMessage::HttpMessage(HeaderSection field_lines, std::string message_body)
+		: field_lines_(field_lines), message_body_(message_body)
+	{}
+
+	bool HttpMessage::operator==(const HttpMessage &rhs) const
+	{
+		return field_lines_ == rhs.field_lines_ && message_body_ == rhs.message_body_;
+	}
+} // namespace http
--- srcs/parse/http/http_message.hpp
+++ srcs/parse/http/http_message.hpp
@@ -15,20 +15,8 @@ namespace http
 		std::string   message_body_;
 
 	  public:
-		HttpMessage(HeaderSection field_lines, std::string message_body)
-			: field_lines_(field_lines), message_body_(message_body)
-		{}
-
-		HttpMessage(std::string request)
-		{
-			(void)request;
-			// TODO : parserは後で別関数に実装
-		}
-
-		bool operator==(const HttpMessage &rhs) const
-		{
-			return field_lines_ == rhs.field_lines_ && message_body_ == rhs.message_body_;
-		}
+		HttpMessage(HeaderSection field_lines, std::string message_body);
+		bool operator==(const HttpMessage &rhs) const;
 	};
 } // namespace http
 #endif
