55340c4b11d31e23bd68023d93898b131076d1f5
nori <nori623p@gmail.com>
2022-11-23 15:10:00 +0100

    fix: 🐛 http名前空間に入れた

--- srcs/parse/http/http_message.hpp
+++ srcs/parse/http/http_message.hpp
@@ -4,27 +4,29 @@
 #include <string>
 #include <vector>
 
-class HttpMessage
+namespace http
 {
-  public:
-  public:
-	HeaderSection field_lines_;
-	std::string   message_body_;
-
-	HttpMessage(HeaderSection field_lines, std::string message_body)
-		: field_lines_(field_lines), message_body_(message_body)
-	{}
-
-	HttpMessage(std::string request)
+	class HttpMessage
 	{
-		(void)request;
-		// TODO : parserは後で別関数に実装
-	}
+	  public:
+	  public:
+		HeaderSection field_lines_;
+		std::string   message_body_;
 
-	bool operator==(const HttpMessage &rhs) const
-	{
-		return field_lines_ == rhs.field_lines_ && message_body_ == rhs.message_body_;
-	}
-};
+		HttpMessage(HeaderSection field_lines, std::string message_body)
+			: field_lines_(field_lines), message_body_(message_body)
+		{}
 
+		HttpMessage(std::string request)
+		{
+			(void)request;
+			// TODO : parserは後で別関数に実装
+		}
+
+		bool operator==(const HttpMessage &rhs) const
+		{
+			return field_lines_ == rhs.field_lines_ && message_body_ == rhs.message_body_;
+		}
+	};
+} // namespace http
 #endif
--- srcs/parse/http/request_message.hpp
+++ srcs/parse/http/request_message.hpp
@@ -4,26 +4,30 @@
 #include "http_message.hpp"
 #include "request_line.hpp"
 
-class RequestMessage : public HttpMessage
+namespace http
 {
-  public:
-	RequestLine request_line_;
-
-	RequestMessage() : HttpMessage(HeaderSection(), "") {}
-
-	RequestMessage(const std::string &str) : HttpMessage(str)
+	class RequestMessage : public HttpMessage
 	{
-		// parse
-	}
+	  public:
+		RequestLine request_line_;
 
-	RequestMessage(RequestLine request_line, HeaderSection field_lines, std::string message_body)
-		: HttpMessage(field_lines, message_body), request_line_(request_line)
-	{}
+		RequestMessage() : HttpMessage(HeaderSection(), "") {}
 
-	bool operator==(const RequestMessage &rhs) const
-	{
-		return HttpMessage::operator==(rhs) && request_line_ == rhs.request_line_;
-	}
-};
+		RequestMessage(const std::string &str) : HttpMessage(str)
+		{
+			// parse
+		}
 
+		RequestMessage(
+			RequestLine request_line, HeaderSection field_lines, std::string message_body
+		)
+			: HttpMessage(field_lines, message_body), request_line_(request_line)
+		{}
+
+		bool operator==(const RequestMessage &rhs) const
+		{
+			return HttpMessage::operator==(rhs) && request_line_ == rhs.request_line_;
+		}
+	};
+} // namespace http
 #endif
