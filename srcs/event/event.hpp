4ace076c9bdf2289bbb30f2efe806848af87635c
nori <nori623p@gmail.com>
2022-11-25 12:23:19 +0100

    refactor: 💡 static constをenumにいれた

--- srcs/event/event.hpp
+++ srcs/event/event.hpp
@@ -10,13 +10,14 @@ namespace event
 	typedef std::list<Event> Events;
 	typedef struct Event {
 		enum EventType {
+			kEmpty = 0,
 			kWrite = 1U << 0,
 			kRead  = 1U << 1
 		};
-		int                   fd;
-		void                 *data;
-		uint32_t              event_type;
-		static const uint32_t kEmpty = 0;
+
+		int      fd;
+		void    *data;
+		uint32_t event_type;
 	} Event;
 } // namespace event
 
