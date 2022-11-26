43c9663835fa07a92c2fe277c5c20018162fabcf
nori <nori623p@gmail.com>
2022-11-26 03:37:10 +0100

    feat: 🎸 requestのパース部分を別クラスに切り出した

---
+++ srcs/server/request_parser.cpp
@@ -0,0 +1,136 @@
+#include "request_parser.hpp"
+#include "debug.hpp"
+#include "http_define.hpp"
+#include "http_exceptions.hpp"
+#include "validate_headers.hpp"
+#include "webserv_utils.hpp"
+
+namespace server
+{
+	RequestParser::RequestParser()
+	{
+		InitParseContext();
+	}
+
+	RequestParser::RequestParser(const RequestParser &other)
+	{
+		*this = other;
+	}
+
+	RequestParser &RequestParser::operator=(const RequestParser &rhs)
+	{
+		if (this == &rhs) {
+			return *this;
+		}
+		state_        = rhs.state_;
+		loaded_data_  = rhs.loaded_data_;
+		*request_ptr_ = *rhs.request_ptr_;
+		return *this;
+	}
+
+	RequestParser::~RequestParser()
+	{
+		delete request_ptr_;
+	}
+
+	bool RequestParser::HasInCompleteData()
+	{
+		return state_ != kStandBy;
+	}
+
+	void RequestParser::InitParseContext()
+	{
+		loaded_data_ = std::string();
+		state_       = kStandBy;
+		request_ptr_ = new http::RequestMessage();
+	}
+
+	Emptiable<Request> RequestParser::Parse(buffer::Buffer &recieved)
+	{
+		if (recieved.empty()) {
+			return Emptiable<Request>();
+		}
+		try {
+			if (CreateRequestMessage(recieved) == kComplete) {
+				Request req = request_ptr_;
+				InitParseContext();
+				return req;
+			}
+			return Emptiable<Request>();
+		} catch (http::HttpException &e) {
+			utils::DeleteSafe(request_ptr_);
+			InitParseContext();
+			return Request(e.GetStatusCode(), Request::kFatal);
+		}
+	}
+
+	// TODO トレイラ無視してる
+	RequestParser::ParseResult RequestParser::CreateRequestMessage(buffer::Buffer &recieved)
+	{
+		switch (state_) {
+		case kStandBy:
+			SetStateAndClearBuf(kStartLine);
+			/* Falls through. */
+		case kStartLine:
+			return ParseStartLine(recieved);
+		case kHeader:
+			return ParseHeaderSection(recieved);
+		case kBody:
+			return ParseBody(recieved);
+		}
+		return kInComplete;
+	}
+
+	RequestParser::ParseResult RequestParser::ParseStartLine(buffer::Buffer &recieved)
+	{
+		if (LoadUntillDelim(recieved, http::kCrLf) != kParsable) {
+			return kInComplete;
+		}
+		loaded_data_.erase(loaded_data_.size() - http::kCrLf.size());
+		request_ptr_->SetRequestLine(RequestLine(loaded_data_));
+		SetStateAndClearBuf(kHeader);
+		return kInComplete;
+	}
+
+	RequestParser::ParseResult RequestParser::ParseHeaderSection(buffer::Buffer &recieved)
+	{
+		if (LoadUntillDelim(recieved, http::kEmptyLine) != kParsable) {
+			return kInComplete;
+		}
+		loaded_data_.erase(loaded_data_.size() - http::kCrLf.size());
+		const HeaderSection headers = HeaderSection(loaded_data_);
+		http::headers::ValidateHeaderSection(headers);
+		request_ptr_->SetHeaderSection(headers);
+		SetStateAndClearBuf(kBody);
+		return request_ptr_->HasMessageBody() ? kInComplete : kComplete;
+	}
+
+	// TODO body
+	RequestParser::ParseResult RequestParser::ParseBody(buffer::Buffer &recieved)
+	{
+		(void)recieved;
+		return kComplete;
+	}
+
+	RequestParser::LoadResult
+	RequestParser::LoadUntillDelim(buffer::Buffer &recieved, const std::string &delim)
+	{
+		for (;;) {
+			if (recieved.empty()) {
+				return kNonParsable;
+			}
+			Emptiable<char> c = recieved.PopChar();
+			loaded_data_ += c.Value();
+			if (utils::EndWith(loaded_data_, delim)) {
+				return kParsable;
+			}
+		}
+	}
+
+	void RequestParser::SetStateAndClearBuf(State new_state)
+	{
+		loaded_data_ = std::string();
+		state_       = new_state;
+	}
+
+} // namespace server
---
+++ srcs/server/request_parser.hpp
@@ -0,0 +1,57 @@
+#ifndef REQUEST_PARSER_HPP
+#define REQUEST_PARSER_HPP
+
+#include <deque>
+
+#include "buffer.hpp"
+#include "emptiable.hpp"
+#include "request.hpp"
+#include "request_message.hpp"
+#include "result.hpp"
+#include "status_code.hpp"
+
+namespace server
+{
+	class RequestParser
+	{
+
+	  private:
+		enum State {
+			kStandBy,
+			kStartLine,
+			kHeader,
+			kBody
+		};
+		enum ParseResult {
+			kComplete,
+			kInComplete
+		};
+		enum LoadResult {
+			kParsable,
+			kNonParsable
+		};
+
+	  private:
+		State                 state_;
+		std::string           loaded_data_;
+		http::RequestMessage *request_ptr_;
+
+	  public:
+		RequestParser();
+		RequestParser(const RequestParser &other);
+		~RequestParser();
+		RequestParser     &operator=(const RequestParser &rhs);
+		Emptiable<Request> Parse(buffer::Buffer &recieved);
+		bool               HasInCompleteData();
+
+	  private:
+		void        InitParseContext();
+		ParseResult CreateRequestMessage(buffer::Buffer &recieved);
+		ParseResult ParseStartLine(buffer::Buffer &recieved);
+		ParseResult ParseHeaderSection(buffer::Buffer &recieved);
+		ParseResult ParseBody(buffer::Buffer &recieved);
+		LoadResult  LoadUntillDelim(buffer::Buffer &recieved, const std::string &delim);
+		void        SetStateAndClearBuf(State new_state);
+	};
+} // namespace server
+#endif
