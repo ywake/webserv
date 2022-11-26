066e6d2ed63c8b85ff9b1e6530112473dcc9847d
nori <nori623p@gmail.com>
2022-11-26 06:18:20 +0100

    fix: 🐛 RequestMessage *やめて値で持つことにした

--- srcs/server/request.hpp
+++ srcs/server/request.hpp
@@ -10,15 +10,15 @@ namespace server
 	class Request : public IRequest
 	{
 	  public:
-		const http::RequestMessage *request_msg_;
-		http::StatusCode            error_code_;
-		ErrorType                   error_type_;
+		http::RequestMessage request_msg_;
+		http::StatusCode     error_code_;
+		ErrorType            error_type_;
 
 	  public:
 		Request(
-			const http::RequestMessage *request_msg = NULL,
+			const http::RequestMessage &request_msg = http::RequestMessage(),
 			const http::StatusCode     &error_code  = http::StatusCode(),
-			ErrorType                   error_type  = kNotError
+			const ErrorType            &error_type  = kNotError
 		);
 		Request(const Request &other);
 		Request(const http::StatusCode &error_code, ErrorType error_type);
