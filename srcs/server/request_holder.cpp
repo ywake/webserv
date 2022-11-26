bc1d5ebef7241b40417ab50297c5733fb09703ac
nori <nori623p@gmail.com>
2022-11-26 04:44:27 +0100

    refactor: 💡 変数名とか関数名変更

---
+++ srcs/server/request_holder.cpp
@@ -0,0 +1,74 @@
+#include "request_holder.hpp"
+#include "debug.hpp"
+#include "http_define.hpp"
+#include "http_exceptions.hpp"
+#include "validate_headers.hpp"
+#include "webserv_utils.hpp"
+
+namespace server
+{
+	RequestHolder::RequestHolder() : request_queue_() {}
+
+	RequestHolder::RequestHolder(const RequestHolder &other)
+	{
+		*this = other;
+	}
+
+	RequestHolder &RequestHolder::operator=(const RequestHolder &rhs)
+	{
+		if (this == &rhs) {
+			return *this;
+		}
+		DeleteAll();
+		for (RequestQueue::const_iterator it = rhs.request_queue_.begin();
+			 it != rhs.request_queue_.end();
+			 it++) {
+			request_queue_.push_back(RequestParser::CopyRequest(*it));
+		}
+		return *this;
+	}
+
+	RequestHolder::~RequestHolder()
+	{
+		DeleteAll();
+	}
+
+	void RequestHolder::DeleteFront()
+	{
+		if (request_queue_.empty()) {
+			log("DeleteRequest: empty");
+		}
+		RequestParser::DestroyRequest(request_queue_.front());
+		request_queue_.pop_front();
+	}
+
+	void RequestHolder::DeleteAll()
+	{
+		while (!request_queue_.empty()) {
+			DeleteFront();
+		}
+	}
+
+	std::size_t RequestHolder::Count()
+	{
+		return request_queue_.size();
+	}
+
+	void RequestHolder::OnEof()
+	{
+		if (parser_.HasInCompleteData()) {
+			request_queue_.push_back(Request(http::StatusCode::kBadRequest, Request::kFatal));
+			parser_.DestroyParseContext();
+		}
+	}
+
+	void RequestHolder::Parse(buffer::Buffer &recieved)
+	{
+		Emptiable<Request> req = parser_.Parse(recieved);
+		if (req.empty()) {
+			return;
+		}
+		request_queue_.push_back(req.Value());
+	}
+
+} // namespace server
