dd5fdf92e13b504ced3eddbed2d1208943dee1e0
nori <nori623p@gmail.com>
2022-11-28 13:48:58 +0100

    Revert "feat: 🎸 RequestMessageがmethodとかを直接持つように変更"
    This reverts commit 384c01e3f07f46ad60b80a43591f16246635a75f.

--- srcs/parse/http/request_message.cpp
+++ srcs/parse/http/request_message.cpp
@@ -2,42 +2,24 @@
 
 namespace http
 {
-	RequestMessage::RequestMessage() : HttpMessage(), method_(), request_target_(), http_version_()
-	{}
+	RequestMessage::RequestMessage() : HttpMessage(), request_line_() {}
 
 	RequestMessage::RequestMessage(const RequestMessage &other) : HttpMessage(other)
 	{
-		method_         = other.method_;
-		request_target_ = other.request_target_;
-		http_version_   = other.http_version_;
+		*this = other;
 	}
 
 	RequestMessage::RequestMessage(
-		const std::string   &method,
-		const RequestTarget &request_target,
-		const std::string   &http_version,
+		const RequestLine   &request_line,
 		const HeaderSection &field_lines,
 		const std::string   &message_body
 	)
-		: HttpMessage(field_lines, message_body),
-		  method_(method),
-		  request_target_(request_target),
-		  http_version_(http_version)
+		: HttpMessage(field_lines, message_body), request_line_(request_line)
 	{}
 
-	void RequestMessage::SetMethod(const std::string &method)
+	void RequestMessage::SetRequestLine(const RequestLine &request_line)
 	{
-		method_ = method;
-	}
-
-	void RequestMessage::SetRequestTarget(const RequestTarget &request_target)
-	{
-		request_target_ = request_target;
-	}
-
-	void RequestMessage::SetHttpVersion(const std::string &http_version)
-	{
-		http_version_ = http_version;
+		request_line_ = request_line;
 	}
 
 	const std::string &RequestMessage::GetMethod() const
@@ -60,17 +42,14 @@ namespace http
 		if (this == &rhs) {
 			return *this;
 		}
+		request_line_ = rhs.request_line_;
 		HttpMessage::operator=(rhs);
-		method_         = rhs.method_;
-		request_target_ = rhs.request_target_;
-		http_version_   = rhs.http_version_;
 		return *this;
 	}
 
 	bool RequestMessage::operator==(const RequestMessage &rhs) const
 	{
-		return HttpMessage::operator==(rhs) && method_ == rhs.method_ &&
-			   request_target_ == rhs.request_target_ && http_version_ == rhs.http_version_;
+		return HttpMessage::operator==(rhs) && request_line_ == rhs.request_line_;
 	}
 
 	RequestMessage::~RequestMessage() {}
