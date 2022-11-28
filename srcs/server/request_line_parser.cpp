ad92f54fdf6f1eba8e973f1caa1fc00fad0f3a9a
nori <nori623p@gmail.com>
2022-11-28 14:51:47 +0100

    feat: 🎸 requestLineParserを実装

---
+++ srcs/server/request_line_parser.cpp
@@ -0,0 +1,190 @@
+#include "request_line_parser.hpp"
+#include "http_define.hpp"
+#include "http_exceptions.hpp"
+#include "method_pool.hpp"
+#include "validate_request_line.hpp"
+
+namespace server
+{
+	const std::size_t RequestLineParser::kMaxRequestTargetSize = 8196;
+
+	RequestLineParser::RequestLineParser()
+	{
+		InitParseContext();
+	}
+
+	RequestLineParser::RequestLineParser(const RequestLineParser &other) : StatefulParser(other)
+	{
+		ctx_.state        = other.ctx_.state;
+		ctx_.request_line = other.ctx_.request_line;
+	}
+
+	RequestLineParser &RequestLineParser::operator=(const RequestLineParser &rhs)
+	{
+		if (this == &rhs) {
+			return *this;
+		}
+		StatefulParser::operator=(rhs);
+		ctx_.state        = rhs.ctx_.state;
+		ctx_.request_line = rhs.ctx_.request_line;
+		return *this;
+	}
+
+	void RequestLineParser::InitParseContext()
+	{
+		ClearLoadedBytes();
+		ctx_.state        = kStandBy;
+		ctx_.request_line = RequestLine();
+	}
+
+	void RequestLineParser::DestroyParseContext()
+	{
+		InitParseContext();
+	}
+
+	bool RequestLineParser::HasInCompleteData()
+	{
+		return ctx_.state != kStandBy;
+	}
+
+	Emptiable<RequestLine> RequestLineParser::Parse(buffer::QueuingBuffer &recieved)
+	{
+		if (recieved.empty()) {
+			return Emptiable<RequestLine>();
+		}
+		try {
+			if (CreateRequestLine(recieved) == kDone) {
+				RequestLine req = ctx_.request_line;
+				InitParseContext();
+				return req;
+			}
+			return Emptiable<RequestLine>();
+		} catch (http::HttpException &e) {
+			DestroyParseContext();
+			throw e;
+		}
+	}
+
+	RequestLineParser::ParseResult
+	RequestLineParser::CreateRequestLine(buffer::QueuingBuffer &recieved)
+	{
+		while (!recieved.empty()) {
+			switch (ParseEachPhase(recieved)) {
+			case kInProgress:
+				return kInProgress;
+			case kDone:
+				if (ctx_.state == kVersion) {
+					return kDone;
+				}
+				ctx_.state = GetNextState(ctx_.state);
+				ClearLoadedBytes();
+			}
+		}
+		return kInProgress;
+	}
+
+	RequestLineParser::ParseResult RequestLineParser::ParseEachPhase(buffer::QueuingBuffer &recieved
+	)
+	{
+		switch (ctx_.state) {
+		case kStandBy:
+			return kInProgress;
+		case kMethod:
+			return ParseMethod(recieved);
+		case kTarget:
+			return ParseRequestTarget(recieved);
+		case kVersion:
+			return ParseHttpVersion(recieved);
+		}
+		return kInProgress;
+	}
+
+	RequestLineParser::ParseResult RequestLineParser::ParseMethod(buffer::QueuingBuffer &recieved)
+	{
+		switch (LoadBytesWithDelim(recieved, http::kSp, http::MethodPool::kMaxMethodLength)) {
+		case kOverMaxSize:
+			throw http::NotImplementedException();
+		case kParsable:
+			loaded_bytes_.erase(loaded_bytes_.size() - http::kSp.size());
+			if (!http::abnf::IsMethod(loaded_bytes_)) {
+				throw http::BadRequestException();
+			}
+			if (!http::MethodPool::Contains(loaded_bytes_)) {
+				throw http::NotImplementedException();
+			}
+			ctx_.request_line.SetMethod(loaded_bytes_);
+			return kDone;
+		case kNonParsable:
+			return kInProgress;
+		}
+		return kInProgress;
+	}
+
+	RequestLineParser::ParseResult
+	RequestLineParser::ParseRequestTarget(buffer::QueuingBuffer &recieved)
+	{
+		switch (LoadBytesWithDelim(recieved, http::kSp, kMaxRequestTargetSize)) {
+		case kOverMaxSize:
+			throw http::UriTooLongException();
+		case kParsable:
+			loaded_bytes_.erase(loaded_bytes_.size() - http::kSp.size());
+			ctx_.request_line.SetRequestTarget(TryConstructRequestTarget(loaded_bytes_));
+			return kDone;
+		case kNonParsable:
+			return kInProgress;
+		}
+		return kInProgress;
+	}
+
+	RequestLineParser::ParseResult
+	RequestLineParser::ParseHttpVersion(buffer::QueuingBuffer &recieved)
+	{
+		switch (LoadBytesWithDelim(recieved, http::kCrLf, http::kHttpVersion.size())) {
+		case kOverMaxSize:
+			throw http::BadRequestException();
+		case kParsable:
+			loaded_bytes_.erase(loaded_bytes_.size() - http::kCrLf.size());
+			if (!http::abnf::IsHttpVersion(loaded_bytes_)) {
+				throw http::BadRequestException();
+			}
+			ctx_.request_line.SetHttpVersion(loaded_bytes_);
+			return kDone;
+		case kNonParsable:
+			return kInProgress;
+		}
+		return kInProgress;
+	}
+
+	RequestTarget RequestLineParser::TryConstructRequestTarget(const ThinString &str)
+	{
+		const std::string &method = ctx_.request_line.GetMethod();
+
+		if (str.empty()) {
+			throw http::BadRequestException();
+		} else if (method == "CONNECT") {
+			return RequestTarget(AuthorityForm(str));
+		} else if (method == "OPTIONS" && str == "*") {
+			return RequestTarget(AsteriskForm(str));
+		} else if (str.at(0) == '/') {
+			return RequestTarget(OriginForm(str));
+		} else {
+			return RequestTarget(AbsoluteForm(str));
+		}
+	}
+
+	RequestLineParser::State RequestLineParser::GetNextState(State old_state)
+	{
+		switch (old_state) {
+		case kStandBy:
+			return kMethod;
+		case kMethod:
+			return kTarget;
+		case kTarget:
+			return kVersion;
+		case kVersion:
+			return kStandBy;
+		}
+		return kStandBy;
+	}
+
+} // namespace server
