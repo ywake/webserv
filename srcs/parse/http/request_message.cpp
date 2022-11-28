384c01e3f07f46ad60b80a43591f16246635a75f
nori <nori623p@gmail.com>
2022-11-28 01:42:36 +0100

    feat: 🎸 RequestMessageがmethodとかを直接持つように変更

--- srcs/parse/http/request_message.cpp
+++ srcs/parse/http/request_message.cpp
@@ -2,24 +2,42 @@
 
 namespace http
 {
-	RequestMessage::RequestMessage() : HttpMessage(), request_line_() {}
+	RequestMessage::RequestMessage() : HttpMessage(), method_(), request_target_(), http_version_()
+	{}
 
 	RequestMessage::RequestMessage(const RequestMessage &other) : HttpMessage(other)
 	{
-		*this = other;
+		method_         = other.method_;
+		request_target_ = other.request_target_;
+		http_version_   = other.http_version_;
 	}
 
 	RequestMessage::RequestMessage(
-		const RequestLine   &request_line,
+		const std::string   &method,
+		const RequestTarget &request_target,
+		const std::string   &http_version,
 		const HeaderSection &field_lines,
 		const std::string   &message_body
 	)
-		: HttpMessage(field_lines, message_body), request_line_(request_line)
+		: HttpMessage(field_lines, message_body),
+		  method_(method),
+		  request_target_(request_target),
+		  http_version_(http_version)
 	{}
 
-	void RequestMessage::SetRequestLine(const RequestLine &request_line)
+	void RequestMessage::SetMethod(const std::string &method)
 	{
-		request_line_ = request_line;
+		method_ = method;
+	}
+
+	void RequestMessage::SetRequestTarget(const RequestTarget &request_target)
+	{
+		request_target_ = request_target;
+	}
+
+	void RequestMessage::SetHttpVersion(const std::string &http_version)
+	{
+		http_version_ = http_version;
 	}
 
 	RequestMessage &RequestMessage::operator=(const RequestMessage &rhs)
@@ -27,14 +45,17 @@ namespace http
 		if (this == &rhs) {
 			return *this;
 		}
-		request_line_ = rhs.request_line_;
 		HttpMessage::operator=(rhs);
+		method_         = rhs.method_;
+		request_target_ = rhs.request_target_;
+		http_version_   = rhs.http_version_;
 		return *this;
 	}
 
 	bool RequestMessage::operator==(const RequestMessage &rhs) const
 	{
-		return HttpMessage::operator==(rhs) && request_line_ == rhs.request_line_;
+		return HttpMessage::operator==(rhs) && method_ == rhs.method_ &&
+			   request_target_ == rhs.request_target_ && http_version_ == rhs.http_version_;
 	}
 
 	RequestMessage::~RequestMessage() {}
--- srcs/parse/http/request_message.hpp
+++ srcs/parse/http/request_message.hpp
@@ -3,25 +3,31 @@
 
 #include "header_section.hpp"
 #include "http_message.hpp"
-#include "request_line.hpp"
+#include "request_target.hpp"
 
 namespace http
 {
 	class RequestMessage : public HttpMessage
 	{
 	  private:
-		RequestLine request_line_;
+		std::string   method_;
+		RequestTarget request_target_;
+		std::string   http_version_;
 
 	  public:
 		RequestMessage();
 		RequestMessage(const RequestMessage &other);
 		RequestMessage(
-			const RequestLine   &request_line,
+			const std::string   &method,
+			const RequestTarget &request_target,
+			const std::string   &http_version,
 			const HeaderSection &field_lines,
 			const std::string   &message_body
 		);
 		~RequestMessage();
-		void            SetRequestLine(const RequestLine &request_line);
+		void            SetMethod(const std::string &method);
+		void            SetRequestTarget(const RequestTarget &request_target);
+		void            SetHttpVersion(const std::string &http_version);
 		RequestMessage &operator=(const RequestMessage &rhs);
 		bool            operator==(const RequestMessage &rhs) const;
 	};
