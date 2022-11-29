08b0fcbaed5440c4786bfce26ef22a533abaeb52
ywake <ywake@student.42tokyo.jp>
2022-11-30 00:25:06 +0900

    feat: 🎸 socketpairの追加

--- cgi-dev/main.cpp
+++ cgi-dev/main.cpp
@@ -2,13 +2,16 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-int child_task()
+int child_task(int fd[2])
 {
+	(void)fd;
+
 	return 0;
 }
 
-void parent_task(int pid)
+void parent_task(int pid, int fd[2])
 {
+	(void)fd;
 	int status;
 
 	waitpid(pid, &status, 0);
@@ -22,17 +25,25 @@ void parent_task(int pid)
 
 int main(void)
 {
+	int fd[2];
+	int ret = socketpair(AF_UNIX, SOCK_STREAM, 0, fd);
+	if (ret == -1) {
+		std::cout << "socketpair error" << std::endl;
+		return 1;
+	}
 	int pid = fork();
 	switch (pid) {
 	case -1:
 		std::cout << "fork error" << std::endl;
 		return 1;
 	case 0:
-		exit(child_task());
+		exit(child_task(fd));
 		break;
 	default:
-		parent_task(pid);
+		parent_task(pid, fd);
 		break;
 	}
+	close(fd[0]);
+	close(fd[1]);
 	return 0;
 }
