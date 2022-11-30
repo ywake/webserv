2b8a0792ff3c2a7f39a95d761395cd973c06b09e
nori <nori623p@gmail.com>
2022-11-30 09:53:59 +0100

    feat: 🎸 BufferedReader::Read()を実装

---
+++ srcs/buffer/buffered_reader.cpp
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+#include "buffered_reader.hpp"
+
+namespace q_buffer
+{
+	ssize_t BufferedReader::Read(int fd)
+	{
+		buf_.push_back(ByteArray(buffer_size_));
+		void   *buf       = buf_.back().data();
+		ssize_t read_size = read(fd, buf, buffer_size_);
+		if (read_size > 0) {
+			buf_.back().resize(read_size);
+		} else {
+			buf_.pop_back();
+		}
+		return read_size;
+	}
+} // namespace q_buffer
