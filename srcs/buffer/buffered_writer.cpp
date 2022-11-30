986effeeed07410d49d42d7e22a4a12e35f48c57
nori <nori623p@gmail.com>
2022-11-30 09:08:54 +0100

    feat: 🎸 BUfferedWriter::Write()を実装

---
+++ srcs/buffer/buffered_writer.cpp
@@ -0,0 +1,27 @@
+#include <cerrno>
+#include <cstring>
+#include <unistd.h>
+
+#include "buffered_writer.hpp"
+
+namespace q_buffer
+{
+	Result<void> BufferedWriter::Write(int fd)
+	{
+		if (QueuingBuffer::empty()) {
+			return Result<void>();
+		}
+		const void *buf        = buf_.front().data() + front_idx_;
+		std::size_t size       = buf_.front().size() - front_idx_;
+		ssize_t     write_size = write(fd, buf, size);
+		if (write_size < 0) {
+			return Error(std::string("write: ") + strerror(errno));
+		}
+		front_idx_ += write_size;
+		if (front_idx_ == buf_.front().size()) {
+			buf_.pop_front();
+			front_idx_ = 0;
+		}
+		return Result<void>();
+	}
+} // namespace q_buffer
