dc7c642feeb5f4bdd4a5a943c88fbdf4b6a0e5f8
nori <nori623p@gmail.com>
2022-12-03 14:35:39 +0100

    feat: 🎸 wip GET

---
+++ srcs/server/response_holder.cpp
@@ -0,0 +1,95 @@
+#include "response_holder.hpp"
+#include "http_exceptions.hpp"
+#include "static_response.hpp"
+
+namespace server
+{
+	inline const std::string &GetHost(const IRequest &request)
+	{
+		return request.Headers()["host"].front().GetValue();
+	}
+
+	event::Instructions ResponseHolder::StartNewResponse(IRequest *request)
+	{
+		in_progress_.push_back(ReqRes(request, NULL));
+		if (request->IsErr()) {
+			// in_progress_.back().second = CreateErrorResponse(code , type, conf)
+			// return Instr(AppendEvent conn write);
+		}
+		IResponse *new_response    = CreateNewResponse(*request);
+		in_progress_.back().second = new_response;
+
+		return CreateInstructionsForNewResopnse(*new_response);
+	}
+
+	IResponse *ResponseHolder::CreateNewResponse(const IRequest &request)
+	{
+		IResponse                *new_response = NULL;
+		const std::string        &host         = GetHost(request);
+		const conf::ServerConf   &vs_conf      = config_[host];
+		const conf::LocationConf &location     = vs_conf.FindMatchingLocationConf(request.Path());
+		bool                      is_cgi       = !location.GetCgiPath().empty();
+		try {
+			if (is_cgi) {
+				new_response = NULL;
+			} else {
+				new_response = new StaticResponse(request, location);
+			}
+		} catch (http::HttpException &e) {
+			delete new_response;
+			new_response = NULL;
+		}
+		return new_response;
+	}
+
+	event::Instructions ResponseHolder::CreateInstructionsForNewResopnse(const IResponse &response)
+	{
+		event::Instructions insts;
+
+		if (response.HasFd()) { // TODO cgiはread | write
+			insts.push_back(event::Instruction(
+				event::Instruction::kRegister, response.GetFd().Value(), event::Event::kRead
+			));
+		}
+		if (response.HasReadyData()) {
+			insts.push_back(event::Instruction(
+				event::Instruction::kAppendEventType, conn_fd_, event::Event::kWrite
+			));
+		}
+		return insts;
+	}
+
+	event::Instructions ResponseHolder::Perform(const event::Event &event)
+	{
+		IResponse *response = in_progress_.front().second;
+		response->Perform(event);
+	}
+
+	Result<event::Instructions> ResponseHolder::Send()
+	{
+		event::Instructions insts;
+
+		IResponse   *response    = in_progress_.front().second;
+		Result<void> send_result = response->Send(conn_fd_);
+		if (send_result.IsErr()) { // TODO conn_fdのwriteを外すべきか？
+			insts.push_back(
+				event::Instruction(event::Instruction::kUnregister, response->GetFd().Value())
+			);
+			delete response;
+			request_del_(in_progress_.front().first);
+			in_progress_.pop_front();
+			return Result<event::Instructions>(insts, send_result.Err());
+		}
+		if (!response->HasReadyData()) {
+			insts.push_back(event::Instruction(
+				event::Instruction::kTrimEventType, conn_fd_, event::Event::kWrite
+			));
+		}
+	}
+
+	std::size_t ResponseHolder::size()
+	{
+		return in_progress_.size();
+	}
+
+} // namespace server
---
+++ srcs/server/response_holder.hpp
@@ -0,0 +1,43 @@
+#ifndef RESPONSE_HOLDER_HPP
+#define RESPONSE_HOLDER_HPP
+
+#include <deque>
+#include <utility>
+
+#include "i_request.hpp"
+#include "i_response.hpp"
+#include "instruction.hpp"
+#include "result.hpp"
+#include "status_code.hpp"
+#include "virtual_server_confs.hpp"
+
+namespace server
+{
+	class ResponseHolder
+	{
+		typedef void (*RequestDelFunc)(IRequest *);
+		typedef std::pair<IRequest *, IResponse *> ReqRes;
+
+	  private:
+		RequestDelFunc                  request_del_;
+		int                             conn_fd_;
+		const conf::VirtualServerConfs &config_;
+		std::deque<ReqRes>              in_progress_;
+
+	  public:
+		ResponseHolder(const conf::VirtualServerConfs &conf, RequestDelFunc del);
+		event::Instructions         StartNewResponse(IRequest *request);
+		event::Instructions         Perform(const event::Event &event);
+		Result<event::Instructions> Send();
+		std::size_t                 size();
+
+	  private:
+		IResponse *CreateNewResponse(const IRequest &request);
+		// IResponse *CreateCgiResponse(const IRequest &request);
+		IResponse *
+		CreateErrorResponse(const http::StatusCode &status_code, IRequest::ErrorType e_type);
+		event::Instructions CreateInstructionsForNewResopnse(const IResponse &response);
+	};
+} // namespace server
+
+#endif
