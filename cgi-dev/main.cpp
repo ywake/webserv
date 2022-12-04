ed84a63347d38bff63894489fa2d723676b26ce2
ywake <ywake@student.42tokyo.jp>
2022-12-04 11:13:37 +0900

    refactor: 💡 関数名をパスカルケースに修正

--- cgi-dev/main.cpp
+++ cgi-dev/main.cpp
@@ -5,11 +5,11 @@
 #include "i_request.hpp"
 #include "location_conf.hpp"
 
-int  err_check(int ret);
-bool is_accessible(const std::string &path);
-void exec_cgi();
-void child_task(int fd[2]);
-void parent_task(int pid, int fd[2]);
+int  ErrCheck(int ret);
+bool IsAccessible(const std::string &path);
+void ExecCgi();
+void ChildTask(int fd[2]);
+void ParentTask(int pid, int fd[2]);
 
 int main(int argc, char *argv[], char *envp[])
 {
@@ -17,19 +17,19 @@ int main(int argc, char *argv[], char *envp[])
 	conf::LocationConf conf;
 
 	std::string resource_path = conf.GetRoot() + request.Path();
-	if (is_accessible(resource_path) == false) {
+	if (IsAccessible(resource_path) == false) {
 		return EXIT_FAILURE; // 404
 	}
-	set_meta_env(resource, envp);
-	char **cmd_line_arg = create_cmd_line_arg(resource);
-	input_body(resource);
-	exec_cgi(cmd_line_arg, envp);
-	read_cgi_output();
-	parse_cgi_output();
+	SetMetaEnv(resource, envp);
+	char **cmd_line_arg = CreateCmdLineArg(resource);
+	InputBody(resource);
+	ExecCgi(cmd_line_arg, envp);
+	ReadCgiOutput();
+	ParseCgiOutput();
 	return 0;
 }
 
-int err_check(int ret)
+int ErrCheck(int ret)
 {
 	if (ret == -1) {
 		std::perror("Error");
@@ -38,7 +38,7 @@ int err_check(int ret)
 	return ret;
 }
 
-bool is_accessible(const std::string &path)
+bool IsAccessible(const std::string &path)
 {
 	if (access(path.c_str(), R_OK) != 0) {
 		return false;
@@ -46,34 +46,34 @@ bool is_accessible(const std::string &path)
 	return true;
 }
 
-void exec_cgi()
+void ExecCgi()
 {
 	int fd[2];
-	err_check(socketpair(AF_UNIX, SOCK_STREAM, 0, fd));
-	int pid = err_check(fork());
+	ErrCheck(socketpair(AF_UNIX, SOCK_STREAM, 0, fd));
+	int pid = ErrCheck(fork());
 	switch (pid) {
 	case 0:
-		child_task(fd);
+		ChildTask(fd);
 		break;
 	default:
-		parent_task(pid, fd);
+		ParentTask(pid, fd);
 		break;
 	}
-	err_check(close(fd[0]));
-	err_check(close(fd[1]));
+	ErrCheck(close(fd[0]));
+	ErrCheck(close(fd[1]));
 }
 
-void child_task(int fd[2])
+void ChildTask(int fd[2])
 {
-	err_check(close(fd[0]));
-	err_check(dup2(fd[1], STDOUT_FILENO));
-	err_check(close(fd[1]));
+	ErrCheck(close(fd[0]));
+	ErrCheck(dup2(fd[1], STDOUT_FILENO));
+	ErrCheck(close(fd[1]));
 
 	char *const argv[] = {(char *)"/usr/bin/python3", (char *)"index.py", NULL};
-	err_check(execve("/usr/bin/python3", argv, NULL));
+	ErrCheck(execve("/usr/bin/python3", argv, NULL));
 }
 
-void parent_task(int pid, int fd[2])
+void ParentTask(int pid, int fd[2])
 {
 	int status;
 
@@ -83,10 +83,10 @@ void parent_task(int pid, int fd[2])
 	else
 		std::cout << "Child exited with unknown status" << std::endl;
 
-	err_check(close(fd[1]));
+	ErrCheck(close(fd[1]));
 
 	char buf[1024];
-	int  res = err_check(read(fd[0], buf, 1023));
+	int  res = ErrCheck(read(fd[0], buf, 1023));
 	buf[res] = '\0';
 	std::cout << buf << std::endl;
 }
