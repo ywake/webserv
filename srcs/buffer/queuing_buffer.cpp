ab53f7ede16aa86b364afd3281c793ef58180b4c
nori <nori623p@gmail.com>
2022-12-06 22:28:55 +0100

    feat: 🎸 queuing bufferにpush_frontを実装

--- srcs/buffer/queuing_buffer.cpp
+++ srcs/buffer/queuing_buffer.cpp
@@ -27,6 +27,26 @@ namespace q_buffer
 		size_ += data.size();
 	}
 
+	void QueuingBuffer::push_front(const std::vector<char> &data)
+	{
+		if (data.empty()) {
+			return;
+		}
+		buf_.push_front(data);
+		size_ += data.size();
+		front_idx_ = 0;
+	}
+
+	void QueuingBuffer::push_front(const std::string &data)
+	{
+		if (data.empty()) {
+			return;
+		}
+		buf_.push_front(ByteArray(data.begin(), data.end()));
+		size_ += data.size();
+		front_idx_ = 0;
+	}
+
 	Emptiable<char> QueuingBuffer::PopChar()
 	{
 		if (empty()) {
