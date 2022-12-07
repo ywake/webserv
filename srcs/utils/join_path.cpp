04fefe151a41546110dd8465ee7fa5acf427bcab
nori <nori623p@gmail.com>
2022-12-07 01:39:34 +0100

    refactor: 💡 rbeginを使うようにした

--- srcs/utils/join_path.cpp
+++ srcs/utils/join_path.cpp
@@ -7,12 +7,9 @@ namespace utils
 		if (parent.empty() || child.empty()) {
 			return parent.empty() ? child : parent;
 		}
-		char parent_back = parent[parent.size() - 1];
-		char child_front = child[0];
-
-		if (parent_back != '/' && child_front != '/') {
+		if (*parent.rbegin() != '/' && *child.begin() != '/') {
 			return parent + '/' + child;
-		} else if (parent_back == '/' && child_front == '/') {
+		} else if (*parent.rbegin() == '/' && *child.begin() == '/') {
 			return parent.substr(0, parent.size() - 1) + child;
 		} else {
 			return parent + child;
