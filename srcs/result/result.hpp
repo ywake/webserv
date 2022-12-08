2daf8b752485f6071a0ae2f19a1e8add7d9fd76b
nori <nori623p@gmail.com>
2022-12-08 18:12:51 +0100

    feat: 🎸 error code返せそうなやつを作った

--- srcs/result/result.hpp
+++ srcs/result/result.hpp
@@ -3,6 +3,106 @@
 
 #include "error.hpp"
 
+namespace result
+{
+	typedef Error *ErrCode;
+
+	template <typename T>
+	class Result
+	{
+	  private:
+		T       val_;
+		ErrCode err_;
+
+	  public:
+		Result() : val_(), err_(){};
+		Result(const T &v) : val_(v), err_(){};
+		Result(const T &v, const ErrCode &e) : val_(v), err_(e){};
+		Result(const ErrCode &e) : val_(), err_(e){};
+		Result(const Result &r) : val_(r.val_), err_(r.err_){};
+		bool IsOk() const
+		{
+			return !IsErr();
+		}
+		bool IsErr() const
+		{
+			return err_ == NULL;
+		}
+		const T &Val() const
+		{
+			return val_;
+		}
+		T &Val()
+		{
+			return val_;
+		}
+		const std::string &ErrMsg() const
+		{
+			return err_->Err();
+		}
+		const ErrCode &Err() const
+		{
+			return err_;
+		}
+		Result &operator=(const Result &r)
+		{
+			if (this != &r) {
+				val_ = r.val_;
+				err_ = r.err_;
+			}
+			return *this;
+		}
+	};
+
+	template <typename T>
+	class Result<T &>
+	{
+	  private:
+		T       default_;
+		T      &val_;
+		ErrCode err_;
+
+	  public:
+		Result() : default_(), val_(default_), err_(){};
+		Result(const T &v) : val_(v), err_(){};
+		Result(const T &v, const ErrCode &e) : val_(v), err_(e){};
+		Result(const ErrCode &e) : default_(), val_(default_), err_(e->Err()){};
+		Result(const Result &r) : val_(r.val_), err_(r.err_){};
+		bool IsOk() const
+		{
+			return !IsErr();
+		}
+		bool IsErr() const
+		{
+			return err_ == NULL;
+		}
+		T &Val()
+		{
+			return val_;
+		}
+		const T &Val() const
+		{
+			return val_;
+		}
+		const std::string &ErrMsg() const
+		{
+			return err_->Err();
+		}
+		const ErrCode &Err() const
+		{
+			return err_;
+		}
+		Result &operator=(const Result &r)
+		{
+			if (this != &r) {
+				val_ = r.val_;
+				err_ = r.err_;
+			}
+			return *this;
+		}
+	};
+} // namespace result
+
 template <typename T>
 class Result
 {
