3fd2f2b861d478c2febc62117ac04a42d1a67a63
nori <nori623p@gmail.com>
2022-12-08 01:43:03 +0100

    feat: 🎸 デバッグ出力用の関数追加

--- srcs/debug.cpp
+++ srcs/debug.cpp
@@ -4,20 +4,58 @@
 #include "epoll.hpp"
 #include "instruction.hpp"
 
-void log(const Error &err)
+#ifdef NDEBUG
+inline void log(const std::string &title)
 {
-	if (DEBUG) {
-		std::cerr << err << std::endl;
-	}
+	(void)title;
+}
+
+inline void loge(const Error &err)
+{
+	(void)err;
+}
+
+inline void loge(const io_multiplexer::ErrEvents &err)
+{
+	(void)err;
+}
+
+#else
+
+void loge(const Error &err)
+{
+	std::cerr << err << std::endl;
 }
 
 void log(const io_multiplexer::ErrEvents &err)
 {
-	if (DEBUG == false) {
-		return;
-	}
 	for (io_multiplexer::ErrEvents::const_iterator it = err.begin(); it != err.end(); ++it) {
 		std::cerr << it->Val() << std::endl;
 		std::cerr << it->Err() << std::endl << std::endl;
 	}
 }
+
+void log(const std::string &title)
+{
+	std::cerr << std::setw(13) << std::left << title << std::endl;
+}
+
+std::string BaseName(std::string s)
+{
+	std::string name;
+	std::size_t pos;
+
+	pos = s.rfind('/');
+	if (pos == std::string::npos) {
+		name = s;
+	} else {
+		name = s.substr(pos + 1, s.size() - pos - 1);
+	}
+
+	pos = name.rfind('.');
+	if (pos == std::string::npos) {
+		return name;
+	}
+	return name.substr(0, pos);
+}
+#endif
--- srcs/debug.hpp
+++ srcs/debug.hpp
@@ -1,23 +1,46 @@
 #ifndef DEBUG_HPP
 #define DEBUG_HPP
 
+#include "epoll.hpp"
+#include "result.hpp"
 #include <iomanip>
 #include <iostream>
 
-#include "epoll.hpp"
-#include "result.hpp"
+#define COL_END "\x1b[0m"
+#define COL_BOLD "\x1b[1m"
+#define COL_THIN "\x1b[2m"
+#define COL_ITALIC "\x1b[3m"
+#define COL_U_LINE "\x1b[4m"
+#define COL_BLACK "\x1b[30m"
+#define COL_RED "\x1b[31m"
+#define COL_GREEN "\x1b[32m"
+#define COL_YELLOW "\x1b[33m"
+#define COL_BLUE "\x1b[34m"
+#define COL_PURPLE "\x1b[35m"
+#define COL_CYAN "\x1b[36m"
+#define COL_WHITE "\x1b[37m"
 
-#define DEBUG true
-
+#ifdef NDEBUG
 template <typename T, typename U>
-void log(T title, U contents)
+inline void log(const T &title, const U &contents, const std::string &delim = "");
+inline void log(const std::string &title);
+inline void loge(const Error &err);
+inline void loge(const io_multiplexer::ErrEvents &err);
+#define DBG_INFO ((void)0)
+#else
+std::string BaseName(std::string s);
+#define DBG_INFO                                                                                   \
+	(std::cerr << COL_BOLD << BaseName(__FILE__) << COL_END << "::" << __func__ << " " << __LINE__ \
+			   << std::endl)
+template <typename T, typename U>
+void log(const T &title, const U &contents, const std::string &delim = "")
 {
-	if (DEBUG) {
-		std::cout << "[ " << std::setw(13) << std::left << title << "] " << contents << std::endl;
-	}
+	std::cerr << "[ " << std::setw(13) << std::left << title << "] " << delim << contents
+			  << std::endl;
 }
 
-void log(const Error &err);
+void log(const std::string &title);
+void loge(const Error &err);
 void log(const io_multiplexer::ErrEvents &err);
-
+#endif
 #endif
