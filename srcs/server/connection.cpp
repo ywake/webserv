3d358d49442e2a8137cf7e0ad6e3f9147f4ee801
nori <nori623p@gmail.com>
2022-12-10 03:34:04 +0100

    fix: 🐛 CLOCK_MONOTONIC_RAWに修正
    あとでposix用に戻すかもしれない

--- srcs/server/connection.cpp
+++ srcs/server/connection.cpp
@@ -37,7 +37,7 @@ namespace server
 		  is_finished_(false),
 		  time_()
 	{
-		clock_gettime(CLOCK_MONOTONIC, &time_);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &time_);
 	}
 
 	// setからeraseするためだけの存在
@@ -91,7 +91,7 @@ namespace server
 		DBG_INFO;
 		event::Instructions insts;
 
-		clock_gettime(CLOCK_MONOTONIC, &time_);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &time_);
 		if (event_type & event::Event::kRead) {
 			log("con recv");
 			Instructions recv_insts = Recieve();
@@ -203,7 +203,7 @@ namespace server
 	bool Connection::IsTimeOut()
 	{
 		struct timespec now;
-		if (clock_gettime(CLOCK_MONOTONIC, &now) == -1) {
+		if (clock_gettime(CLOCK_MONOTONIC_RAW, &now) == -1) {
 			DBG_INFO;
 			log("istimeout clock_gettime", strerror(errno));
 		}
