876f99be7bb721dbd64ae90c715ef5e1b3987f66
nori <nori623p@gmail.com>
2022-12-10 05:35:23 +0100

    fix: 🐛 リテラルを定数で置き換え

--- srcs/http_define.cpp
+++ srcs/http_define.cpp
@@ -7,7 +7,7 @@ namespace http
 	const std::string kWhiteSpaces      = " \t";
 	const std::string kSp               = " ";
 	const std::string kHttpVersion      = "HTTP/1.1";
-	const std::string kContentLength    = "connection";
+	const std::string kConnection       = "connection";
 	const std::string kContentLength    = "content-length";
 	const std::string kTransferEncoding = "transfer-encoding";
 	const std::string kChunked          = "chunked";
