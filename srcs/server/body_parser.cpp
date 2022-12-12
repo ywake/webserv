5dabdd540277857c7816c331161aa54b1736da7c
nori <nori623p@gmail.com>
2022-12-12 05:48:35 +0100

    feat: 🎸 v_serv_confとhostからserverConfを取得する関数を実装、あとで他へ移動したい

--- srcs/server/body_parser.cpp
+++ srcs/server/body_parser.cpp
@@ -2,4 +2,16 @@
 
 namespace server
 {
+	// TODO あとでconfとかに動かしたい
+	const conf::ServerConf &
+	GetServerConf(const conf::VirtualServerConfs &config, const HeaderSection::Values &hosts)
+	{
+		if (hosts.empty()) {
+			return config.GetDefaultServerConf();
+		} else {
+			const std::string &host = hosts.front().GetValue();
+			return config[host];
+		}
+	}
+
 } // namespace server
