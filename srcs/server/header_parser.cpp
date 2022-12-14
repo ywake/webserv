61412b8b66593389386f53cc03b7a8bd40e88838
nori <nori623p@gmail.com>
2022-12-14 01:40:26 +0100

    feat: 🎸 headerをvalidateするようのクラスを追加

---
+++ srcs/server/header_parser.cpp
@@ -0,0 +1,32 @@
+#include "header_parser.hpp"
+#include "webserv_utils.hpp"
+
+namespace server
+{
+	HeaderParser::HeaderParser() : FieldParser() {}
+
+	HeaderParser::HeaderParser(const FieldParser &other) : FieldParser()
+	{
+		*this = other;
+	}
+
+	FieldParser &HeaderParser::operator=(const FieldParser &rhs)
+	{
+		if (&rhs == this) {
+			return *this;
+		}
+		FieldParser::operator=(rhs);
+		return *this;
+	}
+
+	Emptiable<http::FieldSection *> HeaderParser::Parse(q_buffer::QueuingBuffer &recieved)
+	{
+		Emptiable<http::FieldSection *> headers_or_empty = FieldParser::Parse(recieved);
+		if (headers_or_empty.empty()) {
+			return Emptiable<http::FieldSection *>();
+		}
+		http::FieldSection &headers = *headers_or_empty.Value();
+		return &headers;
+	}
+
+} // namespace server
