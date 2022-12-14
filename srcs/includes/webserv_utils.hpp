4f782a9644a59c6c8d128abdc7413fbc994e4b60
nori <nori623p@gmail.com>
2022-12-14 10:38:37 +0100

    feat: 🎸 stringに変換するやつ実装

--- srcs/includes/webserv_utils.hpp
+++ srcs/includes/webserv_utils.hpp
@@ -3,6 +3,7 @@
 
 #include "result.hpp"
 #include "thin_string.hpp"
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -20,6 +21,14 @@ namespace utils
 		delete p;
 		p = NULL;
 	}
+	template <typename T>
+	std::string ToString(const T &data)
+	{
+		std::stringstream ss;
+		ss << data;
+		return ss.str();
+	}
+
 	std::string             ToLowerString(std::string s);
 	Result<long>            StrToLong(const std::string &str);
 	Result<bool>            IsRegularFile(int fd);
