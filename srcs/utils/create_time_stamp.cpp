5d7e1781752599ff387b359fe1c3b6ba510c9e9b
nori <nori623p@gmail.com>
2022-12-16 09:45:03 +0100

    feat: 🎸 timestamp生成関数を追加

---
+++ srcs/utils/create_time_stamp.cpp
@@ -0,0 +1,36 @@
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
+#include <string>
+
+#include "result.hpp"
+
+namespace utils
+{
+	Result<std::string> CreateTimeStamp()
+	{
+		struct timespec ts;
+		struct tm       tm;
+		char            buf[1000];
+
+		if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
+			return Error("clock get: " + std::string(strerror(errno)));
+		}
+		if (localtime_r(&ts.tv_sec, &tm) == NULL) {
+			return Error("localtime_r: " + std::string(strerror(errno)));
+		}
+		sprintf(
+			buf,
+			"%d-%02d-%02d_%02d:%02d:%02d.%09ld",
+			tm.tm_year + 1900,
+			tm.tm_mon + 1,
+			tm.tm_mday,
+			tm.tm_hour,
+			tm.tm_min,
+			tm.tm_sec,
+			ts.tv_nsec
+		);
+		return std::string(buf);
+	}
+} // namespace utils
