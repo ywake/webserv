240567be13369f1275334902599099d463656438
nori <nori623p@gmail.com>
2022-12-16 14:22:56 +0100

    feat: 🎸 get method 実装

---
+++ srcs/response/get_method.hpp
@@ -0,0 +1,32 @@
+#ifndef GET_METHOD_HPP
+#define GET_METHOD_HPP
+
+#include "a_response.hpp"
+#include "location_conf.hpp"
+#include "managed_fd.hpp"
+
+namespace response
+{
+	class GetMethod : public AResponse
+	{
+	  private:
+		const conf::LocationConf &location_;
+		ManagedFd                 managed_fd_;
+
+	  private:
+		GetMethod();
+		GetMethod(const GetMethod &other);
+		GetMethod &operator=(const GetMethod &rhs);
+
+	  public:
+		GetMethod(const server::IRequest &request, const conf::LocationConf &location);
+		void           Perform(const event::Event &event);
+		bool           HasFd() const;
+		Emptiable<int> GetFd() const;
+
+	  private:
+		int TryOpen(const std::string &filename) const;
+	};
+} // namespace response
+
+#endif
