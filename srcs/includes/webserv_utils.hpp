6f710d8763c05704a1fcdfc7f551797b11c845d4
nori <nori623p@gmail.com>
2022-12-19 11:07:55 +0100

    feat: 🎸 上限超えた文字を省略する関数を実装

--- srcs/includes/webserv_utils.hpp
+++ srcs/includes/webserv_utils.hpp
@@ -43,6 +43,8 @@ namespace utils
 	Result<std::string>     NormalizePath(const ThinString &path);
 	Result<std::string>     HttpNormalizePath(const ThinString &path);
 	std::string             JoinPath(const std::string &parent, const std::string &child);
+	std::string
+	OmitExcessBytes(const std::string &s, std::size_t max_width, const std::string &ellipsis);
 } // namespace utils
 
 #endif
