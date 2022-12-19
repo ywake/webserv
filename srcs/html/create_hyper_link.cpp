592f2056458b75c4210bd2798c48729f6e5976e7
nori <nori623p@gmail.com>
2022-12-19 11:10:23 +0100

    feat: 🎸 ハイパーリンク作る関数を実装

---
+++ srcs/html/create_hyper_link.cpp
@@ -0,0 +1,15 @@
+#include "html_utils.hpp"
+#include "parcent_encode.hpp"
+#include "webserv_utils.hpp"
+
+namespace html
+{
+	std::string CreateHyperLink(const std::string &path, const std::string &name)
+	{
+		std::string link_path = uri::PercentEncode(path);
+		std::string link_name = ReplaceHtmlReserved(name);
+		std::string link      = "<a href=\"" + link_path + "\">" + link_name + "</a>";
+		return link;
+	}
+
+} // namespace html
--- srcs/html/html_utils.hpp
+++ srcs/html/html_utils.hpp
@@ -6,6 +6,7 @@
 namespace html
 {
 	std::string ReplaceHtmlReserved(const std::string &s);
+	std::string CreateHyperLink(const std::string &path, const std::string &name);
 
 } // namespace html
 
